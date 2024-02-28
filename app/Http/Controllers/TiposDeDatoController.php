9d516edcdcaf0f69bfbb153ed1d4914c2b7386d2
Brandon <brandon.buendia@jala.university>
2024-02-28 09:23:47 -0500

    se creo el crud de la tabla de tipos_de_datos

---
+++ app/Http/Controllers/TiposDeDatoController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TiposDeDato;
+use Illuminate\Http\Request;
+
+/**
+ * Class TiposDeDatoController
+ * @package App\Http\Controllers
+ */
+class TiposDeDatoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $tiposDeDatos = TiposDeDato::paginate();
+
+        return view('tipos-de-dato.index', compact('tiposDeDatos'))
+            ->with('i', (request()->input('page', 1) - 1) * $tiposDeDatos->perPage());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $tiposDeDato = new TiposDeDato();
+        return view('tipos-de-dato.create', compact('tiposDeDato'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        request()->validate(TiposDeDato::$rules);
+
+        $tiposDeDato = TiposDeDato::create($request->all());
+
+        return redirect()->route('tipos-de-datos.index')
+            ->with('success', 'TiposDeDato created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $tiposDeDato = TiposDeDato::find($id);
+
+        return view('tipos-de-dato.show', compact('tiposDeDato'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $tiposDeDato = TiposDeDato::find($id);
+
+        return view('tipos-de-dato.edit', compact('tiposDeDato'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  TiposDeDato $tiposDeDato
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TiposDeDato $tiposDeDato)
+    {
+        request()->validate(TiposDeDato::$rules);
+
+        $tiposDeDato->update($request->all());
+
+        return redirect()->route('tipos-de-datos.index')
+            ->with('success', 'TiposDeDato updated successfully');
+    }
+
+    /**
+     * @param int $id
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Exception
+     */
+    public function destroy($id)
+    {
+        $tiposDeDato = TiposDeDato::find($id)->delete();
+
+        return redirect()->route('tipos-de-datos.index')
+            ->with('success', 'TiposDeDato deleted successfully');
+    }
+}
---
+++ app/Models/TiposDeDato.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class TiposDeDato
+ *
+ * @property $id
+ * @property $nombre
+ * @property $id_estado
+ * @property $created_at
+ * @property $updated_at
+ *
+ * @property DatosUnicosPorSolicitude[] $datosUnicosPorSolicitudes
+ * @property Estado $estado
+ * @package App
+ * @mixin \Illuminate\Database\Eloquent\Builder
+ */
+class TiposDeDato extends Model
+{
+    
+    static $rules = [
+		'nombre' => 'required',
+		'id_estado' => 'required',
+    ];
+
+    protected $perPage = 20;
+
+    /**
+     * Attributes that should be mass-assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['nombre','id_estado'];
+
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function datosUnicosPorSolicitudes()
+    {
+        return $this->hasMany('App\Models\DatosUnicosPorSolicitude', 'id_tipos_de_datos', 'id');
+    }
+    
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function estado()
+    {
+        return $this->hasOne('App\Models\Estado', 'id', 'id_estado');
+    }
+    
+
+}
