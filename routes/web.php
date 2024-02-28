930d8e1a157dfb1719f7e0962a2338bfa84fcecc
Brandon <brandon.buendia@jala.university>
2024-02-28 11:22:44 -0500

    se agrego la ruta de estados

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CiudadeController;
+use App\Http\Controllers\EstadoController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
@@ -32,3 +33,5 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::resource('ciudades', CiudadeController::class);
+Route::resource('estados', EstadoController::class);
+
