4f53a9bb1b15962b486dfea4cde99a42a5e37ec3
JohanOvi <t0loza0viedoangelj0h4n@gmail.com>
2024-02-29 14:29:36 -0500

    Se agrega el input de contrasena previamente borrado en la vista register.blade

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -74,6 +74,20 @@
                             </div>
                         </div>
 
+                        <div class="row mb-3">
+                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
                         <div class="mb-3 row">
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
 
