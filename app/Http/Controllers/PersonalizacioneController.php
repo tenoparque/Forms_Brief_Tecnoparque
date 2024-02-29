489d421bc13649fab2a88450fadad7db5b443f49
diegoGar05 <dg8903614@gmail.com>
2024-02-29 14:39:48 -0500

    Se agrego comoboBox estados al form de la vita personalizacione

--- app/Http/Controllers/PersonalizacioneController.php
+++ app/Http/Controllers/PersonalizacioneController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Personalizacione;
+use App\Models\Estado;
 use Illuminate\Http\Request;
 
 /**
@@ -32,7 +33,8 @@ class PersonalizacioneController extends Controller
     public function create()
     {
         $personalizacione = new Personalizacione();
-        return view('personalizacione.create', compact('personalizacione'));
+        $estados = Estado::all();
+        return view('personalizacione.create', compact('personalizacione', 'estados'));
     }
 
     /**
--- resources/views/personalizacione/form.blade.php
+++ resources/views/personalizacione/form.blade.php
@@ -27,9 +27,14 @@
             {!! $errors->first('id_users', '<div class="invalid-feedback">:message</div>') !!}
         </div>
         <div class="form-group">
-            {{ Form::label('id_estado') }}
-            {{ Form::text('id_estado', $personalizacione->id_estado, ['class' => 'form-control' . ($errors->has('id_estado') ? ' is-invalid' : ''), 'placeholder' => 'Id Estado']) }}
-            {!! $errors->first('id_estado', '<div class="invalid-feedback">:message</div>') !!}
+            <label for="id_estado">Estado</label>
+            <select name="id_estado" id="id_estado" class="form-control selectpicker"
+            data-style="btn-primary" title="Seleccionar Estado" required>
+                @foreach ($estados as $estado)
+                <!-- We go through the models of the estados that we previously passed through the controller -->
+                    <option value="{{ $estado->id }}">{{ $estado-> nombre }}</option> <!-- We obtain the id and the value -->
+                @endforeach
+            </select>
         </div>
 
     </div>
