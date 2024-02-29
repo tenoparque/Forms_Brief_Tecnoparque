009249ea5bd0cbd6c044ee53e5421efb985ba1d8
deicyjejen21 <deicyjejen21@gmail.com>
2024-02-29 14:55:48 -0500

    imagen login

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,8 +2,7 @@ const images = [
     'images/fondoBrief4.jpg',
     'images/fondo2.jpg',
     'images/fondo3.jpg',
-    
-    // Agregue más rutas de imagen aquí
+    // Agrega más rutas de imagen aquí
 ];
 
 function displayRandomBackgroundImage() {
@@ -12,5 +11,9 @@ function displayRandomBackgroundImage() {
     document.body.style.backgroundRepeat = 'repeat';
 }
 
-displayRandomBackgroundImage(); // Mostrar una imagen de fondo aleatoria inicialmente
-setInterval(displayRandomBackgroundImage, 1000); // Cambiar la imagen de fondo aleatoriamente cada 20 segundos
+// Verifica si estamos en la vista de inicio de sesión (por ejemplo, la URL contiene "/login")
+if (window.location.pathname.includes('/login')) {
+    displayRandomBackgroundImage(); // Mostrar una imagen de fondo aleatoria inicialmente
+    setInterval(displayRandomBackgroundImage, 1000);
+}
+ // Cambiar la imagen de fondo aleatoriamente cada 20 segundos
