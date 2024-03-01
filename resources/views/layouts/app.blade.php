b832807b80e6bc2f6c7e6eaefec5cbb87446b7fb
Darienpes29 <darienpesca29@gmail.com>
2024-03-01 09:23:10 -0500

    Modificación opciones SideBar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,81 +28,45 @@
 </head>
 <body>
     <div class="wrapper">
-        @if(Route::currentRouteName() !== 'login')
+        {{-- @if(Route::currentRouteName() !== 'login') --}}
         <aside id="sidebar">
             <div class="d-flex">
                 <button class="toggle-btn" type="button">
                     <i class="lni lni-grid-alt"></i>
                 </button>
                 <div class="sidebar-logo">
-                    <a href="#">CodzSword</a>
+                    <a href="#">Brief</a>
                 </div>
             </div>
             <ul class="sidebar-nav">
                 <li class="sidebar-item">
                     <a href="#" class="sidebar-link">
                         <i class="lni lni-user"></i>
-                        <span>Profile</span>
+                        <span>Usuario</span>
                     </a>
                 </li>
                 <li class="sidebar-item">
                     <a href="#" class="sidebar-link">
                         <i class="lni lni-agenda"></i>
-                        <span>Task</span>
+                        <span>Rol</span>
                     </a>
                 </li>
-
-
-
-
                 <li class="sidebar-item">
-                    <a href="#" class="sidebar-link  has-dropdown collapsed" data-bs-toggle="collapse"
-                        data-bs-target="#auth" aria-expanded="false" aria-controls="auth">
+                    <a href="#" class="sidebar-link  has-dropdown collapsed">
                         <i class="lni lni-protection"></i>
-                        <span>Auth</span>
+                        <span>Solicitud</span>
                     </a>
-                    <ul id="auth" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
-                        <li class="sidebar-item">
-                            <a href="#" class="sidebar-link">Login</a>
-                        </li>
-                        <li class="sidebar-item">
-                            <a href="#" class="sidebar-link">Register</a>
-                        </li>
-                    </ul>
                 </li>
                 <li class="sidebar-item">
-                    <a href="#" class="sidebar-link collapsed has-dropdown" data-bs-toggle="collapse"
-                        data-bs-target="#multi" aria-expanded="false" aria-controls="multi">
+                    <a href="#" class="sidebar-link collapsed has-dropdown">
                         <i class="lni lni-layout"></i>
-                        <span>Multi Level</span>
+                        <span>Servicios</span>
                     </a>
-                    <ul id="multi" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
-                        <li class="sidebar-item">
-                            <a href="#" class="sidebar-link collapsed" data-bs-toggle="collapse"
-                                data-bs-target="#multi-two" aria-expanded="false" aria-controls="multi-two">
-                                Two Links
-                            </a>
-                            <ul id="multi-two" class="sidebar-dropdown list-unstyled collapse">
-                                <li class="sidebar-item">
-                                    <a href="#" class="sidebar-link">Link 1</a>
-                                </li>
-                                <li class="sidebar-item">
-                                    <a href="#" class="sidebar-link">Link 2</a>
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
                 </li>
                 <li class="sidebar-item">
                     <a href="#" class="sidebar-link">
                         <i class="lni lni-popup"></i>
-                        <span>Notification</span>
-                    </a>
-                </li>
-                <li class="sidebar-item">
-                    <a href="#" class="sidebar-link">
-                        <i class="lni lni-cog"></i>
-                        <span>Setting</span>
+                        <span>Tipo Campo x Solicitud</span>
                     </a>
                 </li>
             </ul>
@@ -113,7 +77,7 @@
                 </a>
             </div>
         </aside>
-        @endif
+        {{-- @endif --}}
 
         <div class="main p-3">
             <div class="text-center">
