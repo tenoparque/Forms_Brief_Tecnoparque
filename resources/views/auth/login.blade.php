d9d10124c86b8d946fb886f9cadc5113e4be9a83
deicyjejen21 <deicyjejen21@gmail.com>
2024-03-01 11:52:38 -0500

    login arreglado

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8" style="width: 40%">
-            <div class="card">
+            <div class="card_login">
                 <div class="card-body" >
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
