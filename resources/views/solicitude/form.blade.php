9cebc26f969bc8006a98ebb88b73bd8582f2964f
Brandon <brandon.buendia@jala.university>
2024-03-04 16:01:40 -0500

    se completo el constructor reemplanzado los combobos por checklist y texbox

--- resources/views/solicitude/form.blade.php
+++ resources/views/solicitude/form.blade.php
@@ -1,3 +1,4 @@
+
 <div class="box box-info padding-1">
     <div class="box-body">
         <div class="form-group">
@@ -9,63 +10,72 @@
                 @endforeach
             </select>
         </div>
-        </div>
-            <!-- Combo box para los servicios asociados -->
-            <div class="form-group" id="servicesComboBoxContainer">
-            <label for="id_servicios_por_tipo">Servicio</label>
-            <select name="id_servicios_por_tipo" id="id_servicios_por_tipo" class="form-control">
-                <!-- Las opciones de los servicios se llenarán dinámicamente aquí -->
-            </select>
-        </div>
+    </div>
 
-         <!-- Combo box para los datos únicos por solicitudes -->
-         <div class="form-group" id="datosUnicosComboBoxContainer">
-            <label for="id_datos_unicos_por_solicitud">Datos Únicos por Solicitud</label>
-            <select name="id_datos_unicos_por_solicitud" id="id_datos_unicos_por_solicitud" class="form-control">
-                <!-- Las opciones de los datos únicos por solicitudes se llenarán dinámicamente aquí -->
-            </select>
-        </div>
+    <!-- Combo box para los servicios asociados -->
+    <div class="form-group" id="servicesComboBoxContainer">
+        <label for="id_servicios_por_tipo">Servicio</label>
+        <!-- Las opciones de los servicios se llenarán dinámicamente aquí -->
+    </div>
+
+    <!-- Textboxes para los datos únicos por solicitudes -->
+    <div class="form-group" id="datosUnicosComboBoxContainer">
+        <h4>Datos Únicos por Solicitud</h4>
+        <!-- Los textboxes se llenarán dinámicamente aquí -->
+    </div>
 </div>
-</div>
+
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script>
     $(document).ready(function () {
-    $('#id_tipos_de_solicitudes').change(function () {
-        var selectedTypeId = $(this).val(); // Obtener el ID del tipo de solicitud seleccionado
-        // Enviar el ID del tipo de solicitud seleccionado a través de una solicitud AJAX al controlador
-        $.ajax({
-            url: '{{ route("solicitude.processSelectedId") }}',
-            type: 'POST',
-            data: {
-                tipo_solicitud_id: selectedTypeId,
-                _token: '{{ csrf_token() }}'
-            },
-            success: function (response) {
-                // Construir el nuevo combo box con los datos únicos por solicitudes obtenidos del controlador
-                var datosUnicosComboBox = '<select name="id_datos_unicos_por_solicitud" class="form-control">';
-                $.each(response.datos_unicos, function (index, datoUnico) {
-                    datosUnicosComboBox += '<option value="' + datoUnico.id + '">' + datoUnico.nombre + '</option>';
-                });
-                datosUnicosComboBox += '</select>';
-                // Mostrar el nuevo combo box en el área designada
-                $('#datosUnicosComboBoxContainer').html(datosUnicosComboBox);
+        $('#id_tipos_de_solicitudes').change(function () {
+            var selectedTypeId = $(this).val(); // Obtener el ID del tipo de solicitud seleccionado
+            // Enviar el ID del tipo de solicitud seleccionado a través de una solicitud AJAX al controlador
+            $.ajax({
+                url: '{{ route("solicitude.processSelectedId") }}',
+                type: 'POST',
+                data: {
+                    tipo_solicitud_id: selectedTypeId,
+                    _token: '{{ csrf_token() }}'
+                },
+                success: function (response) {
+                    // Construir checkboxes para los servicios por solicitud
+                    var serviciosCheckboxes = '';
+                    $.each(response.servicios, function (index, servicio) {
+                        serviciosCheckboxes += '<label><input type="checkbox" name="servicios_por_tipo[]" value="' + servicio.id + '"> ' + servicio.nombre + '</label><br>';
+                    });
+                    // Mostrar los checkboxes en el área designada
+                    $('#servicesComboBoxContainer').html(serviciosCheckboxes);
+                    
+                    // Construir textboxes para los datos únicos por solicitud
+                    var datosUnicosTextboxes = '';
+                    // $.each(response.datos_unicos, function (index, datoUnico) {
+                    //     datosUnicosTextboxes += '<label>' + datoUnico.nombre + '</label>';
+                    //     datosUnicosTextboxes += '<label>' +  ' </label><input type="text" name="datos_unicos_por_solicitud_' + datoUnico.id + '" class="form-control" placeholder="' + datoUnico.nombre + '"><br>';
+                    // });
 
-                // Construir el nuevo combo box con los servicios por solicitud obtenidos del controlador
-                var serviciosComboBox = '<select name="id_servicios_por_tipo" class="form-control">';
-                $.each(response.servicios, function (index, servicio) {
-                    serviciosComboBox += '<option value="' + servicio.id + '">' + servicio.nombre + '</option>';
-                });
-                serviciosComboBox += '</select>';
-                // Mostrar el nuevo combo box en el área designada
-                $('#servicesComboBoxContainer').html(serviciosComboBox);
-            },
-            error: function (xhr) {
-                console.error('Error al obtener los datos asociados al tipo de solicitud.');
-            }
+                    $.each(response.datos_unicos, function (index, datoUnico) {
+                        datosUnicosTextboxes += '<div class="form-group">';
+                        datosUnicosTextboxes += '<div class="row">';
+                        datosUnicosTextboxes += '<div class="col-12 col-md-6">';
+                        datosUnicosTextboxes += '<label>' + datoUnico.nombre + '</label>';
+                        datosUnicosTextboxes += '</div>';
+                        datosUnicosTextboxes += '<div class="col-12 col-md-6">';
+                        datosUnicosTextboxes += '<input type="text" name="datos_unicos_por_solicitud_' + datoUnico.id + '" class="form-control" placeholder="' + datoUnico.nombre + '">';
+                        datosUnicosTextboxes += '</div>';
+                        datosUnicosTextboxes += '</div>';
+                        datosUnicosTextboxes += '</div>';
+});
+
+                   
+
+                    // Mostrar los textboxes en el área designada
+                    $('#datosUnicosComboBoxContainer').html(datosUnicosTextboxes);
+                },
+                error: function (xhr) {
+                    console.error('Error al obtener los datos asociados al tipo de solicitud.');
+                }
+            });
         });
     });
-});
 </script>
-    
-</div>
-
