df77e606347a2cf62330067bc86eea90189f28fa
JohanOvi <t0loza0viedoangelj0h4n@gmail.com>
2024-03-05 10:34:45 -0500

    Se realiza la funcionalidad de busqueda en vivo en la vista index.blade de los Roles

--- resources/views/role/index.blade.php
+++ resources/views/role/index.blade.php
@@ -32,6 +32,9 @@
                     <div class="card-body">
                         <div class="table-responsive">
                             <table class="table table-striped table-hover">
+                                <div class="card-header">
+                                    <input class="form-control" id="search" placeholder="Ingrese el nombre del Rol...">
+                                </div>
                                 <thead class="thead">
                                     <tr>
                                         <th>No</th>
@@ -42,13 +45,13 @@
                                         <th></th>
                                     </tr>
                                 </thead>
-                                <tbody>
+                                <tbody class="alldata">
                                     @foreach ($roles as $role)
                                         <tr>
                                             <td>{{ ++$i }}</td>
                                             
 											<td>{{ $role->name }}</td>
-											<td>{{ $role->guard_name }}</td>
+											<!-- <td>{{ $role->guard_name }}</td> -->
 
                                             <td>
                                                 <form action="{{ route('roles.destroy',$role->id) }}" method="POST">
@@ -60,6 +63,10 @@
                                         </tr>
                                     @endforeach
                                 </tbody>
+                                <!-- Another tbody is created for the search records -->
+                                <tbody id="Content" class="dataSearched">
+                                    
+                                </tbody>
                             </table>
                         </div>
                     </div>
@@ -68,4 +75,34 @@
             </div>
         </div>
     </div>
+
+    <!-- JS Scripts -->
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script>
+        // javascript and ajax code
+        $('#search').on('keyup',function()
+        {
+            $value=$(this).val();
+
+            if ($value) {
+                $('.alldata').hide();
+                $('.dataSearched').show();
+            } else {
+                $('.alldata').show();
+                $('.dataSearched').hide();
+            }
+
+            $.ajax({
+                type: 'get',
+                url: "{{ URL::to('searchRol') }}",
+                data:{'search': $value},
+
+                success:function(data)
+                {
+                    $('#Content').html(data);
+                }
+            });
+        })
+    </script>
 @endsection
