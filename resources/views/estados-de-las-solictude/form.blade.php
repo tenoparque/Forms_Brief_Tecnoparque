7d93d346c72f072f65e5cfb3eb72110d4b284120
diegoGar05 <dg8903614@gmail.com>
2024-03-06 16:27:22 -0500

    Se modifico en form estados de las solicitudes para que el combo estados tenga seleccionado el registro guardado

--- resources/views/estados-de-las-solictude/form.blade.php
+++ resources/views/estados-de-las-solictude/form.blade.php
@@ -13,7 +13,9 @@
             data-style="btn-primary" title="Seleccionar Estado" required>
                 @foreach ($estados as $estado)
                 <!-- We go through the models of the estado that we previously passed through the controller -->
-                    <option value="{{ $estado->id }}">{{ $estado-> nombre }}</option> <!-- We obtain the id and the value -->
+                <option value="{{ $estado->id }}" {{ ($estadosDeLasSolictude->id_estado ?? '') == $estado->id ? 'selected' : '' }}>
+                    {{ $estado->nombre }}
+                </option> <!-- We obtain the id and the value -->
                 @endforeach
             </select>
         </div>
