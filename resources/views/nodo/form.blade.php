cc6a62001a0129f8b8075a6579c545784210154f
diegoGar05 <dg8903614@gmail.com>
2024-03-06 15:19:38 -0500

    Se agrego condicional en form nodo para que muestre combo ciudades solo en create

--- resources/views/nodo/form.blade.php
+++ resources/views/nodo/form.blade.php
@@ -19,17 +19,22 @@
                 </select>
             </div>
         @endif
-        <div class="form-group">
-            <label for="id_ciudad">Ciudad</label>
-            <select name="id_ciudad" id="id_ciudad" class="form-control selectpicker"
-            data-style="btn-primary" title="Seleccionar la Ciudad" required>
-                @foreach ($ciudades as $ciudad)
-                <!-- We go through the models of the ciudades that we previously passed through the controller -->
-                    <option value="{{ $ciudad->id }}">{{ $ciudad-> nombre }}</option> <!-- We obtain the id and the value -->
-                @endforeach
-            </select>
-        </div>
+        @if(Route::currentRouteName() === 'nodos.create')
+            <div class="form-group">
+                <label for="id_ciudad">Ciudad</label>
+                <select name="id_ciudad" id="id_ciudad" class="form-control selectpicker"
+                data-style="btn-primary" title="Seleccionar la Ciudad" required>
+                    @foreach ($ciudades as $ciudad)
+                    <!-- We go through the models of the ciudades that we previously passed through the controller -->
+                        <option value="{{ $ciudad->id }}">{{ $ciudad-> nombre }}</option> <!-- We obtain the id and the value -->
+                    @endforeach
+                </select>
+            </div>
+        @endif
     </div>
+
+
+
     <div class="box-footer mt20">
         <button type="submit" class="btn btn-primary">{{ __('Submit') }}</button>
     </div>
