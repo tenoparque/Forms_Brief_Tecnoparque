40ffe6247fdd0e3e92fe00f6895a750232d72106
Brandon <brandon.buendia@jala.university>
2024-03-06 11:11:45 -0500

    se modifico el encabezado del formulario de eventos especiales por categoria en la vista create

--- resources/views/eventos-especiales-por-categoria/create.blade.php
+++ resources/views/eventos-especiales-por-categoria/create.blade.php
@@ -37,7 +37,14 @@
 
                 <div class="card card-default">
                     <div class="card-header">
-                        <span class="card-title">{{ __('Create') }} Eventos Especiales Por Categoria</span>
+                        <div class="d-flex mt-3 mb-4">
+                            <div>
+                                <h1 class="primeraPalabraFlex">{{ __('CREAR EVENTO') }}</h1>
+                            </div>
+                            <div>
+                                <h1 class="segundaPalabraFlex">{{ __('ESPECIAL POR CATEGORIA') }}</h1>
+                            </div>
+                        </div>
                     </div>
                     <div class="card-body">
                         <form method="POST" action="{{ route('eventos-especiales-por-categorias.store') }}"  role="form" enctype="multipart/form-data">
