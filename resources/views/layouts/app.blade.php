a5cbb3348ff9324fcb5cc25d379ccfff7b3cd145
deicyjejen21 <deicyjejen21@gmail.com>
2024-03-06 10:05:08 -0500

    modificacion tabla departamento

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+    
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -31,8 +32,10 @@
     @vite(['resources/sass/app.scss', 'resources/js/app.js', 'resources/img'])
     @vite('resources/js/menuburger.js')
     @vite('resources/js/validateUserRegister.js')
+   
 </head>
 <body>
+
     <div class="wrapper">
         @if(Route::currentRouteName() !== 'login')
         <aside id="sidebar">
