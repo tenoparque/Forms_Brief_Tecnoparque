d55bf3429a709575e62f001bb992c74c4e3cd7f5
Brandon <brandon.buendia@jala.university>
2024-03-06 11:14:34 -0500

    se modifico el encabezado de la tabla de categoria de eventos especiales con el tipo y color de letra

--- resources/views/categorias-eventos-especiale/index.blade.php
+++ resources/views/categorias-eventos-especiale/index.blade.php
@@ -12,9 +12,14 @@
                     <div class="card-header">
                         <div style="display: flex; justify-content: space-between; align-items: center;">
 
-                            <span id="card_title">
-                                {{ __('Categorias Eventos Especiale') }}
-                            </span>
+                            <div class="d-flex mt-3 mb-4">
+                                <div>
+                                    <h1 class="primeraPalabraFlex">{{ __('CATEGORIAS') }}</h1>
+                                </div>
+                                <div>
+                                    <h1 class="segundaPalabraFlex">{{ __('DE EVENTOS ESPECIALES') }}</h1>
+                                </div>
+                            </div>
 
                              <div class="float-right">
                                 <a href="{{ route('categorias-eventos-especiales.create') }}" class="btn btn-primary btn-sm float-right"  data-placement="left">
