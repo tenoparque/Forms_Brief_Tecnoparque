38954a6417044a20fdbdd1f6c67ec23520e3c201
diegoGar05 <dg8903614@gmail.com>
2024-03-06 14:58:37 -0500

    Se modifico en el form de nodo el combo que muestra estado para que en el edit muestre el estado del nodo.

--- resources/views/nodo/form.blade.php
+++ resources/views/nodo/form.blade.php
@@ -7,16 +7,17 @@
             {!! $errors->first('nombre', '<div class="invalid-feedback">:message</div>') !!}
         </div>
         @if(Route::currentRouteName() === 'nodos.edit')
-        <div class="form-group">
-            <label for="id_estado">Estado</label>
-            <select name="id_estado" id="id_estado" class="form-control selectpicker"
-            data-style="btn-primary" title="Seleccionar Estado" required>
-                @foreach ($estados as $estado)
-                <!-- We go through the models of the estados that we previously passed through the controller -->
-                    <option value="{{ $estado->id }}">{{ $estado-> nombre }}</option> <!-- We obtain the id and the value -->
-                @endforeach
-            </select>
-        </div>
+            <div class="col-md-4">
+                <label for="id_estado">Estado</label>
+                <select name="id_estado" id="id_estado" class="form-control selectpicker"
+                data-style="btn-primary" title="Seleccionar Estado" required>
+                    @foreach ($estados as $estado)
+                        <option value="{{ $estado->id }}" {{ ($nodo->id_estado ?? '') == $estado->id ? 'selected' : '' }}>
+                            {{ $estado->nombre }}
+                        </option> 
+                    @endforeach
+                </select>
+            </div>
         @endif
         <div class="form-group">
             <label for="id_ciudad">Ciudad</label>
