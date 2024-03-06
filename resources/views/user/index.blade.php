35ffad4d55f7fff7c4b96471c31f3e1f43a50e02
deicyjejen21 <deicyjejen21@gmail.com>
2024-03-06 11:15:30 -0500

    modificacion  index /  users

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -5,60 +5,78 @@
 @endsection
 
 @section('content')
-<div class="container-fluid" style="background-image: url('{{ asset('images/fondoBrief4.jpg') }}'); background-position: center; height: 100vh;">
-    <div class="container-fluid">
+    <header class="container-fluid mt-5">
+        <div class="row d-flex justify-content-between" style="align-items: center; margin-top: 60px">
+            <div class="col-md-4 col-sm-6 mb-3 mb-sm-0">
+                <div class="">
+                    <div class="text-wel">
+                        <h5 class="welcoRe">BIENVENIDO</h5>
+                        <div class="d-flex">
+                            <h2 class="supereh">SUPER - ‎ </h2>
+                            <h2 class="adminreh"> ADMIN</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-8 ">
+                <img class="redtecnocol" src="/images/recursos/redtecnocol.png"></img>
+            </div>
+        </div>
+    </header>
+    <div class="container mt-5">
         <div class="row">
             <div class="col-sm-12">
-                <div class="card-container mt-3"> <!-- Contenedor para aplicar el margen superior -->
-                    <div class="card-index">
-                    
-                            <div style="display: flex; justify-content: space-between; align-items: center;">
+                <div class="card" style="">
+                    <div class="card-header">
+                        <div style="display: flex; justify-content: space-between; align-items: center;">
 
-                                <div class="d-flex">
-                                    <div class="">
-                                        <h1 class="UsuariosFlex">{{ __('USUARIOS ') }}</h1>
-                                    </div>
-                                    <div class="">
-                                        <h1 class="RegisterFlex">{{ __('REGISTRADOS ') }}</h1>
-                                    </div>
-                        
+                            <div class="d-flex">
+                                <div>
+                                    <h1 class="primeraPalabraFlex" style="margin-right: 0; font-size:180%">
+                                        {{ __('USUARIOS ‎') }}</h1>
                                 </div>
-                                <div class="float-right">
-                                    <a href="{{ route('register') }}" class="btn btn-primary btn-sm float-right"  data-placement="left">
+                                <div>
+                                    <h1 class="segundaPalabraFlex" style="margin-left: 0; font-size:180%">
+                                        {{ __('REGISTRADOS') }}</h1>
+                                </div>
+                            </div>
+                            <div class="float-right">
+                                <a href="{{ route('departamentos.create') }}" class="btn btn-primary btn-sm float-right"
+                                    data-placement="left">
                                     {{ __('Create New') }}
-                                    </a>
-                                </div>
+                                </a>
                             </div>
                         </div>
-                        @if ($message = Session::get('success'))
-                            <div class="alert alert-success">
-                                <p>{{ $message }}</p>
-                            </div>
-                        @endif
+                    </div>
+                    @if ($message = Session::get('success'))
+                        <div class="alert alert-success">
+                            <p>{{ $message }}</p>
+                        </div>
+                    @endif
 
-                        <div class="card-body">
-                            <div class="table-responsive">
-                                <table class="table table-striped table-hover">
-                                    <thead class="thead">
-                                        <tr>
-                                            <th>No</th>
-                                            
-                                            <th>Name</th>
-                                            <th>Email</th>
-                                            <th>Celular</th>
-                                            <th>Apellidos</th>
-                                            <th>Nodo</th>
-                                            <th>Estado</th>
-                                            <th>Rol</th>
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table table-striped table-hover">
+                                <div class="card-header">
+                                    <tr>
+                                        <th>No</th>
 
-                                            <th></th>
-                                        </tr>
+                                        <th>Name</th>
+                                        <th>Email</th>
+                                        <th>Celular</th>
+                                        <th>Apellidos</th>
+                                        <th>Nodo</th>
+                                        <th>Estado</th>
+                                        <th>Rol</th>
+
+                                        <th></th>
+                                    </tr>
                                     </thead>
                                     <tbody>
                                         @foreach ($users as $user)
                                             <tr>
                                                 <td>{{ ++$i }}</td>
-                                                
+
                                                 <td>{{ $user->name }}</td>
                                                 <td>{{ $user->email }}</td>
                                                 <td>{{ $user->celular }}</td>
@@ -66,7 +84,7 @@
                                                 <td>{{ $user->nodo->nombre }}</td>
                                                 <td>{{ $user->estado->nombre }}</td>
                                                 <td>
-                                                    @foreach($user->roles as $role)
+                                                    @foreach ($user->roles as $role)
                                                         {{ $role->name }}
                                                         @if (!$loop->last)
                                                             <br>
@@ -75,24 +93,29 @@
                                                 </td>
 
                                                 <td>
-                                                    <form action="{{ route('users.destroy',$user->id) }}" method="POST">
-                                                        <a class="btn btn-sm btn-primary " href="{{ route('users.show',$user->id) }}"><i class="fa fa-fw fa-eye"></i> {{ __('Show') }}</a>
-                                                        <a class="btn btn-sm btn-success" href="{{ route('users.edit',$user->id) }}"><i class="fa fa-fw fa-edit"></i> {{ __('Edit') }}</a>
+                                                    <form action="{{ route('users.destroy', $user->id) }}" method="POST">
+                                                        <a class="btn btn-sm btn-primary "
+                                                            href="{{ route('users.show', $user->id) }}"><i
+                                                                class="fa fa-fw fa-eye"></i> {{ __('Show') }}</a>
+                                                        <a class="btn btn-sm btn-success"
+                                                            href="{{ route('users.edit', $user->id) }}"><i
+                                                                class="fa fa-fw fa-edit"></i> {{ __('Edit') }}</a>
                                                         @csrf
                                                         @method('DELETE')
-                                                        <button type="submit" class="btn btn-danger btn-sm"><i class="fa fa-fw fa-trash"></i> {{ __('Delete') }}</button>
+                                                        <button type="submit" class="btn btn-danger btn-sm"><i
+                                                                class="fa fa-fw fa-trash"></i> {{ __('Delete') }}</button>
                                                     </form>
                                                 </td>
                                             </tr>
                                         @endforeach
                                     </tbody>
-                                </table>
-                            </div>
-                       
+                            </table>
+                        </div>
                     </div>
                 </div>
-                {!! $users->links() !!}
             </div>
+            {!! $users->links() !!}
         </div>
+    </div>
     </div>
 @endsection
