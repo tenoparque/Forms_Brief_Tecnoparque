82da20246f86cdcc9dcf7200ed6a6d36abac5fd3
diegoGar05 <dg8903614@gmail.com>
2024-03-06 11:38:24 -0500

    Modificaciones EventosEspecialesporCategoria: Se modico en el modelo el rules para que idEstado no sea required, en el controller store se agrego que guarde por prederminado id estado = , en el form se agrego condicional para que no muestre el combo "estado" en el create

--- resources/views/eventos-especiales-por-categoria/form.blade.php
+++ resources/views/eventos-especiales-por-categoria/form.blade.php
@@ -6,6 +6,7 @@
             {{ Form::text('nombre', $eventosEspecialesPorCategoria->nombre, ['class' => 'form-control' . ($errors->has('nombre') ? ' is-invalid' : ''), 'placeholder' => 'Nombre']) }}
             {!! $errors->first('nombre', '<div class="invalid-feedback">:message</div>') !!}
         </div>
+        @if(Route::currentRouteName() === 'eventos-especiales-por-categorias.edit')
         <div class="form-group">
             <label for="id_estado">Estado</label>
             <select name="id_estado" id="id_estado" class="form-control selectpicker"
@@ -16,6 +17,7 @@
                 @endforeach
             </select>
         </div>
+        @endif
         <div class="form-group">
             <label for="id_eventos_especiales">categoria</label>
             <select name="id_eventos_especiales" id="id_eventos_especiales" class="form-control selectpicker"
