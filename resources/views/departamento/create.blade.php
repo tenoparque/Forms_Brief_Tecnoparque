522ae6b833e85a0913330dde67d81254526ede25
Brandon <brandon.buendia@jala.university>
2024-03-06 10:25:26 -0500

    se modifico el encabezado del formulario de creacion de departamentos en la vista departamentos create y se la agrego la letra y colores establecidos

--- resources/views/departamento/create.blade.php
+++ resources/views/departamento/create.blade.php
@@ -37,7 +37,14 @@
 
                 <div class="card card-default">
                     <div class="card-header">
-                        <span class="card-title">{{ __('Create') }} Departamento</span>
+                        <div class="d-flex">
+                            <div>
+                                <h1 class="primeraPalabraFlex">{{ __('CREAR') }}</h1>
+                            </div>
+                            <div>
+                                <h1 class="segundaPalabraFlex" >{{ __('DEPARTAMENTO') }}</h1>
+                            </div>
+                        </div>
                     </div>
                     <div class="card-body">
                         <form method="POST" action="{{ route('departamentos.store') }}"  role="form" enctype="multipart/form-data">
