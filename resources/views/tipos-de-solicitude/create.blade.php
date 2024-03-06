518cfd6aaaee5da8e3429db147824f63d0cad884
Brandon <brandon.buendia@jala.university>
2024-03-06 10:34:46 -0500

    se modifico el nombre del encabezado de la tabla en la vista de tipos de solicitudes create agregando el nombre y el color de la letra correspondiente

--- resources/views/tipos-de-solicitude/create.blade.php
+++ resources/views/tipos-de-solicitude/create.blade.php
@@ -13,7 +13,15 @@
 
                 <div class="card card-default">
                     <div class="card-header">
-                        <span class="card-title">{{ __('Create') }} Tipos De Solicitude</span>
+                        <div class="d-flex">
+                            <div class="">
+                                <h1 class="primeraPalabraFlex">{{ __('CREAR TIPO ') }}</h1>
+                            </div>
+                            <div class="">
+                                <h1 class="segundaPalabraFlex">{{ __('DE SOLICITUD') }}</h1>
+                            </div>
+                
+                        </div>
                     </div>
                     <div class="card-body">
                         <form method="POST" action="{{ route('tipos-de-solicitudes.store') }}"  role="form" enctype="multipart/form-data">
