37bedb5fd7ab1a08eef7e9b0414bbd89236b1507
Darienpes29 <darienpesca29@gmail.com>
2024-03-07 17:17:07 -0500

    Se modificó el diseño de la tabla Roles en el index

--- resources/views/role/index.blade.php
+++ resources/views/role/index.blade.php
@@ -5,27 +5,24 @@
 @endsection
 
 @section('content')
-
-    <div class="container mt-5">
-        <div class="row">
-            <div class="col-sm-12">
-                <div class="card">
+    <section class="container shadow p-4 my-5 bg-light rounded">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
                     <div class="card-header">
-                        <div style="display: flex; justify-content: space-between; align-items: center;">
+                        <div style="d-flex justify-content-between align-items-center">
 
-                            <div class="d-flex mt-3 mb-4">
-                                <div>
-                                    <h1 class="primeraPalabraFlex" style="margin-right: 0; font-size: 180%; font-weight: 900; color: rgb(0, 49, 77)">{{ __('ROLES') }}</h1>
+                            <div style="d-flex justify-content-between align-items-center">
+
+                                <div class="d-flex mt-3 mb-4">
+                                    <div>
+                                        <h1 class="primeraPalabraFlex"
+                                            style="margin-right: 0; font-size: 180%; font-weight: 900; color: rgb(0, 49, 77)">
+                                            {{ __('ROLES') }}</h1>
+                                    </div>
 
                                 </div>
-                                
                             </div>
-
-                             <div class="float-right">
-                                <a href="{{ route('roles.create') }}" class="btn btn-outline"
-                                    style="color:#00324D; border:2px solid #82DEF0; height: 40px; width:120px; cursor: pointer;  border-radius: 35px; justify-content: center; justify-items: center; ">{{ __('CREAR') }}
-                                    <i class="fa-solid fa-circle-play" style="color: #642c78;"></i></a>
-                              </div>
                         </div>
                     </div>
                     @if ($message = Session::get('success'))
@@ -35,34 +32,42 @@
                     @endif
 
                     <div class="card-body">
-                        <div class="table-responsive">
-                            <table class="table table-striped table-hover">
-                                <div class="card-header">
-                                    <input class="form-control" id="search" placeholder="Ingrese el nombre del Rol...">
-                                </div>
-                                <thead class="thead">
-                                    <tr>
+                        <div class="row mb-3">
+                            <div class="col d-flex justify-content-between align-items-center">
+                                <input class="form-control" id="search" placeholder="Ingrese el nombre del usuario..."
+                                    style="width: 70%; border-radius: 50px; border-style: solid; border-width:4px; border-color: #DEE2E6">
+                                <a href="{{ route('register') }}" class="btn btn-outline"
+                                    style="color:#00324D; border:2px solid #82DEF0; height: 40px; width:120px; cursor: pointer;  border-radius: 35px; justify-content: center; justify-items: center; ">{{ __('CREAR') }}
+                                    <i class="fa-solid fa-circle-play" style="color: #642c78;"></i></a>
+                            </div>
+                        </div>
+                        <div class="table-responsive"
+                            style="background-color: #DEE2E6; border-radius: 18px; border-style: solid; border-width:2px; border-color: #DEE2E6">
+                            <table class="table table-bordered table-hover">
+                                <thead class="thead-dark">
+                                    <tr style="border-width: 2px">
                                         <th>No</th>
-                                        
-										<th>Nombre</th>
-										<!-- <th>Guard Name</th> -->
-
-                                        <th></th>
+                                        <th>Nombre</th>
+                                        <th>Opciones</th>
                                     </tr>
                                 </thead>
                                 <tbody class="alldata">
                                     @foreach ($roles as $role)
                                         <tr>
                                             <td>{{ ++$i }}</td>
-                                            
-											<td>{{ $role->name }}</td>
-											<!-- <td>{{ $role->guard_name }}</td> -->
+
+                                            <td>{{ $role->name }}</td>
+                                            <!-- <td>{{ $role->guard_name }}</td> -->
 
                                             <td>
-                                                <form action="{{ route('roles.destroy',$role->id) }}" method="POST">
-                                                    <a class="btn btn-sm btn-primary " href="{{ route('roles.show',$role->id) }}"><i class="fa fa-fw fa-eye"></i> {{ __('Show') }}</a>
-                                                    <a class="btn btn-sm btn-success" href="{{ route('roles.edit',$role->id) }}"><i class="fa fa-fw fa-edit"></i> {{ __('Edit') }}</a>
-                                                    
+                                                <form action="{{ route('roles.destroy', $role->id) }}" method="POST">
+                                                    <a class="btn btn-sm btn-primary "
+                                                        href="{{ route('roles.show', $role->id) }}"><i
+                                                            class="fa fa-fw fa-eye"></i> {{ __('Detalle') }}</a>
+                                                    <a class="btn btn-sm btn-success"
+                                                        href="{{ route('roles.edit', $role->id) }}"><i
+                                                            class="fa fa-fw fa-edit"></i> {{ __('Editar') }}</a>
+
                                                 </form>
                                             </td>
                                         </tr>
@@ -70,25 +75,25 @@
                                 </tbody>
                                 <!-- Another tbody is created for the search records -->
                                 <tbody id="Content" class="dataSearched">
-                                    
+
                                 </tbody>
                             </table>
                         </div>
                     </div>
+
                 </div>
                 {!! $roles->links() !!}
             </div>
         </div>
-    </div>
+    </section>
 
     <!-- JS Scripts -->
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script>
         // javascript and ajax code
-        $('#search').on('keyup',function()
-        {
-            $value=$(this).val();
+        $('#search').on('keyup', function() {
+            $value = $(this).val();
 
             if ($value) {
                 $('.alldata').hide();
@@ -101,10 +106,11 @@
             $.ajax({
                 type: 'get',
                 url: "{{ URL::to('searchRol') }}",
-                data:{'search': $value},
+                data: {
+                    'search': $value
+                },
 
-                success:function(data)
-                {
+                success: function(data) {
                     $('#Content').html(data);
                 }
             });
