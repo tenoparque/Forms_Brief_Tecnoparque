892da8914685470a59c014b78bad8a52e1b26c01
deicyjejen21 <deicyjejen21@gmail.com>
2024-03-07 14:45:04 -0500

    cierre de etiqueta

--- resources/views/departamento/form.blade.php
+++ resources/views/departamento/form.blade.php
@@ -9,6 +9,6 @@
     <div class="box-footer mt20">
         <button type="submit" class="btn btn-outline" <a href="{{ route('departamentos.create') }}" class="btn btn-outline"
         style="color:#00324D; border:2px solid #82DEF0; height: 40px; width:120px; cursor: pointer; margin-left: 90%; border-radius: 35px; margin-top:10px; justify-content: center; justify-items: center; ">{{ __('GUARDAR') }}
-        <i class="fa-solid fa-circle-plus fa-sm" style="color: #642c78;"></i>
+        <i class="fa-solid fa-circle-plus fa-sm" style="color: #642c78;"></i></button>
     </div>
 </div>
