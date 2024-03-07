f21c701321d82b501de3a41b72253642a94cd0dd
diegoGar05 <dg8903614@gmail.com>
2024-03-07 08:00:19 -0500

    Se modificaron y se agregarin los combos al form servicios por tipos de solicitudes para que en el combo de edit este seleccionado el dato correspondiente

--- resources/views/servicios-por-tipos-de-solicitude/form.blade.php
+++ resources/views/servicios-por-tipos-de-solicitude/form.blade.php
@@ -6,6 +6,7 @@
             {{ Form::text('nombre', $serviciosPorTiposDeSolicitude->nombre, ['class' => 'form-control' . ($errors->has('nombre') ? ' is-invalid' : ''), 'placeholder' => 'Nombre']) }}
             {!! $errors->first('nombre', '<div class="invalid-feedback">:message</div>') !!}
         </div>
+        @if(Route::currentRouteName() === 'servicios-por-tipos-de-solicitudes.create')
         <div class="form-group">
             <label for="id_tipo_de_solicitud">Tipo de Solicitud</label>
             <select name="id_tipo_de_solicitud" id="id_tipo_de_solicitud" class="form-control selectpicker"
@@ -16,19 +17,37 @@
                 @endforeach
             </select>
         </div>
+        @endif
 
         @if(Route::currentRouteName() === 'servicios-por-tipos-de-solicitudes.edit')
-        <div class="form-group">
-            <label for="id_estado">Estado</label>
-            <select name="id_estado" id="id_estado" class="form-control selectpicker"
-            data-style="btn-primary" title="Seleccionar el Estado" required>
-                @foreach ($estados as $estado)
-                <!-- We go through the models of the estados that we previously passed through the controller -->
-                    <option value="{{ $estado->id }}">{{ $estado-> nombre }}</option> <!-- We obtain the id and the value -->
-                @endforeach
-            </select>
-        </div>
+            <div class="form-group">
+                <label for="id_tipo_de_solicitud">Tipo de Solicitud</label>
+                <select name="id_tipo_de_solicitud" id="id_tipo_de_solicitud" class="form-control selectpicker"
+                data-style="btn-primary" title="Seleccionar Tipo de Solicitud" required>
+                    @foreach ($solicitudes as $solicitud)
+                        <option value="{{ $solicitud->id }}" {{ ($serviciosPorTiposDeSolicitude->id_tipo_de_solicitud ?? '') == $solicitud->id ? 'selected' : '' }}>
+                            {{ $solicitud->nombre }}
+                        </option>
+                    @endforeach
+                </select>
+            </div>
         @endif
+
+        @if(Route::currentRouteName() === 'servicios-por-tipos-de-solicitudes.edit')
+            <div class="form-group">
+                <label for="id_estado">Estado</label>
+                <select name="id_estado" id="id_estado" class="form-control selectpicker"
+                data-style="btn-primary" title="Seleccionar el Estado" required>
+                    @foreach ($estados as $estado)
+                        <option value="{{ $estado->id }}" {{ ($serviciosPorTiposDeSolicitude->id_estado ?? '') == $estado->id ? 'selected' : '' }}>
+                            {{ $estado->nombre }}
+                        </option>
+                    @endforeach
+                </select>
+            </div>
+        @endif
+
+
 
 
     </div>
