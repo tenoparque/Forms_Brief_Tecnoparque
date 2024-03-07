f19f39ec0ab5a6b98a3d89450e26f9e2c770d4c1
Darienpes29 <darienpesca29@gmail.com>
2024-03-07 17:07:01 -0500

    Se modificó la vista del detalle del rol

--- resources/views/role/show.blade.php
+++ resources/views/role/show.blade.php
@@ -6,39 +6,53 @@
 
 @section('content')
 
-    <section class="content container mt-5">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="card">
+    <section class="container shadow p-4 my-5 bg-light rounded">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
                     <div class="card-header">
                         <div class="float-left">
-                            <div class="d-flex mt-3 mb-4">
+                            <div class="d-flex mt-3 mb-2">
                                 <div>
-                                    <h1 class="primeraPalabraFlex" style="font-size: 180%" >{{ __('DETALLES') }}</h1>
+                                    <h1 class="primeraPalabraFlex" style="font-size: 180%" >{{ __('DETALLES DEL') }}</h1>
                                 </div>
                                 <div>
-                                    <h1 class="segundaPalabraFlex" style="font-size: 180%">{{ __('DEL ROL') }}</h1>
+                                    <h1 class="segundaPalabraFlex" style="font-size: 180%">{{ __('ROL') }}</h1>
                                 </div>
                             </div>
-                        </div>
-                        <div class="float-right">
-                            <a class="btn btn-primary" href="{{ route('roles.index') }}"> {{ __('Back') }}</a>
                         </div>
                     </div>
 
                     <div class="card-body">
-                        
-                        <div class="form-group">
-                            <strong>Name:</strong>
-                            {{ $role->name }}
+                        <div class="row mb-3">
                         </div>
-                        <div class="form-group">
-                            <strong>Guard Name:</strong>
-                            {{ $role->guard_name }}
+                        <div class="table-responsive"
+                            style="background-color: #DEE2E6; border-radius: 18px; border-style: solid; border-width:2px; border-color: #DEE2E6">
+                            <table class="table table-bordered table-hover">
+                                <thead class="thead-dark">
+                                    <tr style="border-width: 2px">
+                                        <th>Nombre</th>
+                                    </tr>
+                                </thead>
+                                <tbody class="alldata">
+                                    <tr>
+                                        <td>{{ $role->name }}</td>
+                                    </tr>
+                                </tbody>
+                                <!-- Another tbody is created for the search records -->
+                                <tbody id="Content" class="dataSearched">
+
+                                </tbody>
+                            </table>
                         </div>
 
                     </div>
+
                 </div>
+            </div>
+            <div class="float-right" style="
+            text-align: right; margin-top: 10px">
+                <a class="btn btn-primary" href="{{ route('roles.index') }}"> {{ __('Regresar') }}</a>
             </div>
         </div>
     </section>
