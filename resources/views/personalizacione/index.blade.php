7a91ea06bf139ee7bb9d4fc43ffc02a5e813985b
Brandon <brandon.buendia@jala.university>
2024-03-08 11:39:40 -0500

    se modifico el estilo de la tabla en la vista index de personalizaciones ajustandola al diseño

--- resources/views/personalizacione/index.blade.php
+++ resources/views/personalizacione/index.blade.php
@@ -5,30 +5,23 @@
 @endsection
 
 @section('content')
+<link rel="stylesheet" href="{{ asset('css/layout.css') }}">
 
-    <div class="container mt-5">
-        <div class="row">
-            <div class="col-sm-12">
-                <div class="card">
+    
+    <section class="container shadow p-4 my-5 bg-light rounded">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
                     <div class="card-header">
-                        <div style="display: flex; justify-content: space-between; align-items: center;">
-
+                        <div class="d-flex justify-content-between align-items-center">
+                            
                             <div class="d-flex mt-3 mb-4">
                                 <div>
-                                    <h1 class="primeraPalabraFlex" style="margin-right: 0;font-size: 180%" >{{ __('PERS') }}</h1>
+                                    <h1 class="primeraPalabraFlex" style="margin-right: 0; font-size: 180%; font-weight: 900; color: rgb(0, 49, 77)">{{ __('PERSONALIZACIONES') }}</h1>
                                 </div>
-                                <div>
-                                    <h1 class="segundaPalabraFlex" style="margin-left:0 ;font-size: 180%">{{ __('ONALIZACIONES') }}</h1>
-                                </div>
+
                             </div>
-
-                             <div class="float-right">
-
-                                    <a href="{{ route('personalizaciones.create') }}" class="btn btn-outline"
-                                    style="color:#00324D; border:2px solid #82DEF0; height: 40px; width:120px; cursor: pointer;  border-radius: 35px; justify-content: center; justify-items: center; ">{{ __('CREAR') }}
-                                    <i class="fa-solid fa-circle-play" style="color: #642c78;"></i></a>
-                                
-                              </div>
+                            
                         </div>
                     </div>
                     @if ($message = Session::get('success'))
@@ -36,11 +29,18 @@
                             <p>{{ $message }}</p>
                         </div>
                     @endif
-
                     <div class="card-body">
-                        <div class="table-responsive">
-                            <table class="table table-striped table-hover">
-                                <thead class="thead">
+                        <div class="row mb-3">
+                            <div class="col d-flex justify-content-between align-items-center">
+                                <input class="form-control" id="search" placeholder="Ingrese el nombre deL nodo..." style="width: 70%; border-radius: 50px; border-style: solid; border-width:4px; border-color: #DEE2E6">
+                                <a href="{{ route('personalizaciones.create') }}" class="btn btn-outline"
+                                    style="color:#00324D; border:2px solid #82DEF0; height: 40px; width:120px; cursor: pointer;  border-radius: 35px; justify-content: center; justify-items: center; ">{{ __('CREAR') }}
+                                    <i class="fa-solid fa-circle-play" style="color: #642c78;"></i></a>
+                            </div>
+                        </div>
+                        <div class="table-responsive" style="background-color: #DEE2E6; border-radius: 18px; border-style: solid; border-width:2px; border-color: #DEE2E6">
+                            <table class="table table-bordered table-hover">
+                                <thead class="thead-dark">
                                     <tr>
                                         <th>No</th>
                                         
@@ -51,26 +51,41 @@
 										<th>Usuario</th>
 										<th>Estado</th>
 
-                                        <th></th>
+                                        <th>Opciones</th>
                                     </tr>
                                 </thead>
-                                <tbody>
+                                <tbody class="alldata">
                                     @foreach ($personalizaciones as $personalizacione)
-                                        <tr>
-                                            <td>{{ ++$i }}</td>
+                                    <tr>
+                                        <td>{{ ++$i }}</td>
+                                        
+                                        <td>{{ $personalizacione->logo }}</td>
+                                        <td>{{ $personalizacione->color_principal }}</td>
+                                        <td>{{ $personalizacione->color_secundario }}</td>
+                                        <td>{{ $personalizacione->color_terciario }}</td>
+                                        <td>{{ $personalizacione->user->name }}</td>
+                                        <td>{{ $personalizacione->estado->nombre }}</td>
+                                        <td> 
+                                           
+                                            <a href="{{ route('personalizaciones.show',$personalizacione->id) }}" class="btn btn-outline"
+                                                style="color:#00324D; border:2px solid #82DEF0; height: 40px; width:100px; cursor: pointer; border-radius: 35px; justify-content: center; justify-items: center; position: relative;"
+                                                onmouseover="this.style.backgroundColor='#b2ebf2';"
+                                                onmouseout="this.style.backgroundColor='#FFFF';">
+                                                {{ __('Detalle') }}
+                                                <i class="fa-sharp fa-solid fa-eye fa-xs" style="color: #642c78; margin-left: 5px;"></i>
+                                            </a>
                                             
-											<td>{{ $personalizacione->logo }}</td>
-											<td>{{ $personalizacione->color_principal }}</td>
-											<td>{{ $personalizacione->color_secundario }}</td>
-											<td>{{ $personalizacione->color_terciario }}</td>
-											<td>{{ $personalizacione->user->name }}</td>
-											<td>{{ $personalizacione->estado->nombre }}</td>
-
-                                            <td>
-                                                    <a class="btn btn-sm btn-primary " href="{{ route('personalizaciones.show',$personalizacione->id) }}"><i class="fa fa-fw fa-eye"></i> {{ __('Show') }}</a>
-                                                    <a class="btn btn-sm btn-success" href="{{ route('personalizaciones.edit',$personalizacione->id) }}"><i class="fa fa-fw fa-edit"></i> {{ __('Edit') }}</a>
-                                            </td>
-                                        </tr>
+                                            <a href="{{ route('personalizaciones.edit',$personalizacione->id) }}" class="btn btn-outline"
+                                                style="color:#00324D; border:2px solid #82DEF0; height: 40px; width:100px; cursor: pointer; border-radius: 35px; justify-content: center; justify-items: center; position: relative;"
+                                                onmouseover="this.style.backgroundColor='#b2ebf2';"
+                                                onmouseout="this.style.backgroundColor='#FFFF';">
+                                                {{ __('Editar') }}
+                                                <i class="fa-solid fa-pen-to-square fa-xs" style="color: #39a900;"></i>
+                                            </a>
+                                        
+                                        
+                                        </td>
+                                    </tr>
                                     @endforeach
                                 </tbody>
                             </table>
