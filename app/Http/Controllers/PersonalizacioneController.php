45a1c29a83b3077065c4b82786dce5705f306332
Brandon <brandon.buendia@jala.university>
2024-03-08 14:56:25 -0500

    se modifico el contrlador el controlador de personalizacoiones con el metodo que devuelve la consulta para que la barra de busqeuda de la vista index funciones, tambien se agrego en la vista index la solicitud ajax

--- app/Http/Controllers/PersonalizacioneController.php
+++ app/Http/Controllers/PersonalizacioneController.php
@@ -27,6 +27,37 @@ class PersonalizacioneController extends Controller
             ->with('i', (request()->input('page', 1) - 1) * $personalizaciones->perPage());
     }
 
+    public function search(Request $request)
+    {
+        $output= ""; // The output variable is defined and initialized
+        $personalizaciones = Personalizacione::where('id_users', 'LIKE', '%'.$request -> search.'%')->get(); // We make the query through the Ciudad name
+
+        // We use the loop foreach to iterate the aggregation of records
+        foreach($personalizaciones as $personalizacion){
+            $output .= 
+            '<tr>
+                <td>' . $personalizacion->id . '</td>
+                <td>' . $personalizacion->logo . '</td>
+                <td>' . $personalizacion->color_principal . '</td>
+                <td>' . $personalizacion->color_secundario . '</td>
+                <td>' . $personalizacion->color_terciario . '</td>
+                <td>' . $personalizacion->users->name . '</td>
+                <td>' . $personalizacion->estado->nombre . '</td>
+                
+                <td>
+                    <a href="' . url('/personalizaciones/' . $personalizacion->id) . '" class="btn btn-sm btn-primary">
+                        <i class="fa fa-fw fa-eye"></i> Show
+                    </a>
+                    <a href="' . url('/personalizaciones/' . $personalizacion->id . '/edit') . '" class="btn btn-sm btn-success">
+                        <i class="fa fa-fw fa-edit"></i> Edit
+                    </a>
+                </td>
+            </tr>';
+        }
+
+        return response($output); // We return the response by sending as parameter the output variable
+    }
+
     /**
      * Show the form for creating a new resource.
      *
