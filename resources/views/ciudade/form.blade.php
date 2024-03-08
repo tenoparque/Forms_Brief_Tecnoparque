bd8a2b7ca5050a7752e331f6da699efaeb5dc9d5
deicyjejen21 <deicyjejen21@gmail.com>
2024-03-08 11:58:36 -0500

    hover botones ciudades

--- resources/views/ciudade/form.blade.php
+++ resources/views/ciudade/form.blade.php
@@ -36,7 +36,9 @@
     </div>
     <div class="box-footer mt20" style="text-align: right;">
         <button type="submit" class="btn btn-outline"
-            style="color:#00324D; border:2px solid #82DEF0; height: 40px; width:120px; cursor: pointer; border-radius: 35px; margin-top:10px; justify-content: center; justify-items: center; margin-left: 0;">
+            style="color:#00324D; border:2px solid #82DEF0; height: 40px; width:120px; cursor: pointer; border-radius: 35px; margin-top:10px; justify-content: center; justify-items: center; margin-left: 0;" 
+            onmouseover="this.style.backgroundColor='#b2ebf2';"
+            onmouseout="this.style.backgroundColor='#FFFF';">
             {{ __('GUARDAR') }}
             <i class="fa-solid fa-circle-plus fa-sm" style="color: #642c78;"></i>
         </button>
