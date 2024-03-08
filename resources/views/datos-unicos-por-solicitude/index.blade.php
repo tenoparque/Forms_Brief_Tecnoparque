008fcfc95f3b5e4b5525142ea1032d5407a336ac
Brandon <brandon.buendia@jala.university>
2024-03-08 10:30:29 -0500

    se modifico el estilo de la tabla en la vista index de datos unicos por solicitudes

--- resources/views/datos-unicos-por-solicitude/index.blade.php
+++ resources/views/datos-unicos-por-solicitude/index.blade.php
@@ -1,34 +1,30 @@
 @extends('layouts.app')
 
 @section('template_title')
-    Datos Unicos Por Solicitude
+    Ciudade
 @endsection
 
 @section('content')
+<link rel="stylesheet" href="{{ asset('css/layout.css') }}">
 
-    <div class="container mt-5">
-        <div class="row">
-            <div class="col-sm-12">
-                <div class="card">
+    
+    <section class="container shadow p-4 my-5 bg-light rounded">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
                     <div class="card-header">
-                        <div style="display: flex; justify-content: space-between; align-items: center;">
-
+                        <div class="d-flex justify-content-between align-items-center">
+                            
                             <div class="d-flex mt-3 mb-4">
                                 <div>
                                     <h1 class="primeraPalabraFlex" style="font-size: 180%">{{ __('DATOS UNICOS') }}</h1>
                                 </div>
                                 <div>
-                                    <h1 class="segundaPalabraFlex" style="font-size: 180%">{{ __('POR TIPO DE SOLICITUD') }}</h1>
+                                    <h1 class="segundaPalabraFlex" style="font-size: 180%">{{ __(' POR TIPO DE SOLICITUD') }}</h1>
                                 </div>
-                            </div>
 
-                             <div class="float-right">
-                                <a href="{{ route('datos-unicos-por-solicitudes.create') }}" class="btn btn-outline"
-                                style="color:#00324D; border:2px solid #82DEF0; height: 40px; width:120px; cursor: pointer;  border-radius: 35px; justify-content: center; justify-items: center; ">{{ __('CREAR') }}
-                                <i class="fa-solid fa-circle-play" style="color: #642c78;"></i></a>
-                                
-    
-                              </div>
+                            </div>
+                            
                         </div>
                     </div>
                     @if ($message = Session::get('success'))
@@ -38,24 +34,31 @@
                     @endif
 
                     <div class="card-body">
-                        <div class="table-responsive">
-                            <table class="table table-striped table-hover">
-                                <thead class="thead">
-                                    <tr>
+                        <div class="row mb-3">
+                            <div class="col d-flex justify-content-between align-items-center">
+                                <input class="form-control" id="search" placeholder="Ingrese el nombre de la categoria de eventos especiales..." style="width: 70%; border-radius: 50px; border-style: solid; border-width:4px; border-color: #DEE2E6">
+                                <a href="{{ route('datos-unicos-por-solicitudes.create') }}" class="btn btn-outline"
+                                    style="color:#00324D; border:2px solid #82DEF0; height: 40px; width:120px; cursor: pointer;  border-radius: 35px; justify-content: center; justify-items: center; ">{{ __('CREAR') }}
+                                    <i class="fa-solid fa-circle-play" style="color: #642c78;"></i></a>
+                            </div>
+                        </div>
+                        <div class="table-responsive" style="background-color: #DEE2E6; border-radius: 18px; border-style: solid; border-width:2px; border-color: #DEE2E6">
+                            <table class="table table-bordered table-hover">
+                                <thead class="thead-dark">
+                                    <tr style="border-width: 2px">
                                         <th>No</th>
-                                        
 										<th>Nombre</th>
 										<th>Tipo De Dato</th>
 										<th>Tipo De Solicitud</th>
 										<th>Estado</th>
 
-                                        <th></th>
+                                        <th>Opciones</th>
                                     </tr>
                                 </thead>
-                                <tbody>
+                                <tbody class="alldata">
                                     @foreach ($datosUnicosPorSolicitudes as $datosUnicosPorSolicitude)
-                                        <tr>
-                                            <td>{{ ++$i }}</td>
+                                    <tr>
+                                        <td>{{ ++$i }}</td>
                                             
 											<td>{{ $datosUnicosPorSolicitude->nombre }}</td>
 											<td>{{ $datosUnicosPorSolicitude->tiposDeDato->nombre }}</td>
@@ -63,6 +66,7 @@
 											<td>{{ $datosUnicosPorSolicitude->estado->nombre }}</td>
 
                                             <td> 
+                    
                                                 <a href="{{ route('datos-unicos-por-solicitudes.show' ,$datosUnicosPorSolicitude->id) }}" class="btn btn-outline"
                                                 style="color:#00324D; border:2px solid #82DEF0; height: 40px; width:100px; cursor: pointer; border-radius: 35px; justify-content: center; justify-items: center; position: relative;"
                                                 onmouseover="this.style.backgroundColor='#b2ebf2';"
