46c2adec70d90a97baacf1010a6cb6ce5c02d0ef
JohanOvi <t0loza0viedoangelj0h4n@gmail.com>
2024-03-08 10:55:22 -0500

    Se actualiza el diseño de la pagina index.blade de nodos y se modifica el nodoController para devolver el numero del registro en vez del id

--- resources/views/nodo/index.blade.php
+++ resources/views/nodo/index.blade.php
@@ -5,7 +5,6 @@
 @endsection
 
 @section('content')
-    
 
     <section class="container shadow p-4 my-5 bg-light rounded">
         <div class="container">
@@ -15,39 +14,37 @@
                         <div class="d-flex justify-content-between align-items-center">
                             
                             <div class="d-flex mt-3 mb-4">
-                                <div style="margin-right: 0;" >
+                                <div>
                                     <h1 class="primeraPalabraFlex" style="margin-right: 0; font-size: 180%; font-weight: 900; color: rgb(0, 49, 77)">{{ __('NODOS') }}</h1>
-                                   
                                 </div>
 
                             </div>
                             
-                            <!-- <a href="{{ route('nodos.create') }}" class="btn btn-primary btn-sm">{{ __('Create New') }}</a> -->
                         </div>
                     </div>
                     @if ($message = Session::get('success'))
-                    <div class="alert alert-success">
-                        <p>{{ $message }}</p>
-                    </div>
+                        <div class="alert alert-success">
+                            <p>{{ $message }}</p>
+                        </div>
                     @endif
 
                     <div class="card-body">
                         <div class="row mb-3">
                             <div class="col d-flex justify-content-between align-items-center">
-                                <input class="form-control" id="search" placeholder="Ingrese el nombre del Nodo..." style="width: 70% ;">
+                                <input class="form-control" id="search" placeholder="Ingrese el nombre deL nodo..." style="width: 70%; border-radius: 50px; border-style: solid; border-width:4px; border-color: #DEE2E6">
                                 <a href="{{ route('nodos.create') }}" class="btn btn-outline"
-                                style="color:#00324D; border:2px solid #82DEF0; height: 40px; width:120px; cursor: pointer;  border-radius: 35px; justify-content: center; justify-items: center; ">{{ __('CREAR') }}
-                                <i class="fa-solid fa-circle-play" style="color: #642c78;"></i></a>
+                                    style="color:#00324D; border:2px solid #82DEF0; height: 40px; width:120px; cursor: pointer;  border-radius: 35px; justify-content: center; justify-items: center; ">{{ __('CREAR') }}
+                                    <i class="fa-solid fa-circle-play" style="color: #642c78;"></i></a>
+                            </div>
                         </div>
-                        <div class="table-responsive">
+                        <div class="table-responsive" style="background-color: #DEE2E6; border-radius: 18px; border-style: solid; border-width:2px; border-color: #DEE2E6">
                             <table class="table table-bordered table-hover">
                                 <thead class="thead-dark">
                                     <tr>
                                         <th scope="col">No</th>
                                         <th scope="col">Nombre</th>
-                                        <th scope="col">Estado</th>
                                         <th scope="col">Ciudad</th>
-                                        <th scope="col"></th>
+                                        <th scope="col">Opciones</th>
                                     </tr>
                                 </thead>
                                 <tbody class="alldata">
@@ -55,11 +52,26 @@
                                     <tr>
                                         <td>{{ ++$i }}</td>
                                         <td>{{ $nodo->nombre }}</td>
-                                        <td>{{ $nodo->estado->nombre }}</td>
                                         <td>{{ $nodo->ciudade->nombre }}</td>
                                         <td> 
-                                            <a class="btn btn-sm btn-primary mr-2" href="{{ route('nodos.show',$nodo->id) }}"><i class="fa fa-fw fa-eye"></i> {{ __('Detalle') }}</a>
-                                            <a class="btn btn-sm btn-success" href="{{ route('nodos.edit',$nodo->id) }}"><i class="fa fa-fw fa-edit"></i> {{ __('Editar') }}</a>
+                                           
+                                            <a href="{{ route('nodos.show' ,$nodo->id) }}" class="btn btn-outline"
+                                                style="color:#00324D; border:2px solid #82DEF0; height: 40px; width:100px; cursor: pointer; border-radius: 35px; justify-content: center; justify-items: center; position: relative;"
+                                                onmouseover="this.style.backgroundColor='#b2ebf2';"
+                                                onmouseout="this.style.backgroundColor='#FFFF';">
+                                                {{ __('Detalle') }}
+                                                <i class="fa-sharp fa-solid fa-eye fa-xs" style="color: #642c78; margin-left: 5px;"></i>
+                                            </a>
+                                            
+                                            <a href="{{ route('nodos.edit' , $nodo->id) }}" class="btn btn-outline"
+                                                style="color:#00324D; border:2px solid #82DEF0; height: 40px; width:100px; cursor: pointer; border-radius: 35px; justify-content: center; justify-items: center; position: relative;"
+                                                onmouseover="this.style.backgroundColor='#b2ebf2';"
+                                                onmouseout="this.style.backgroundColor='#FFFF';">
+                                                {{ __('Editar') }}
+                                                <i class="fa-solid fa-pen-to-square fa-xs" style="color: #39a900;"></i>
+                                            </a>
+                                        
+                                        
                                         </td>
                                     </tr>
                                     @endforeach
@@ -71,7 +83,9 @@
                             </table>
                         </div>
                     </div>
+
                 </div>
+                {!! $nodos->links() !!}
             </div>
         </div>
     </section>
