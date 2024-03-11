feb8be660c23f3f80ea2e24c257d2932b83ec655
deicyjejen21 <deicyjejen21@gmail.com>
2024-03-11 10:41:01 -0500

    modificacion de la tabla detalle de categoria por eventos especiales

--- resources/views/eventos-especiales-por-categoria/show.blade.php
+++ resources/views/eventos-especiales-por-categoria/show.blade.php
@@ -5,45 +5,59 @@
 @endsection
 
 @section('content')
-
-    <section class="content container mt-5">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="card">
+    <section class="container shadow p-4 my-5 bg-light rounded">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
                     <div class="card-header">
                         <div class="float-left">
-                            <div class="d-flex mt-3 mb-4">
+                            <div class="d-flex mt-3 mb-2">
                                 <div>
-                                    <h1 class="primeraPalabraFlex" style="font-size:180%">{{ __('DETALLE DEL EVENTO') }}</h1>
+                                    <h1 class="primeraPalabraFlex" style="font-size:200%">{{ __('DETALLE DEL EVENTO') }}</h1>
                                 </div>
                                 <div>
-                                    <h1 class="segundaPalabraFlex" style="font-size: 180%">{{ __('ESPECIAL POR CATEGORIA') }}</h1>
+                                    <h1 class="segundaPalabraFlex" style="font-size: 200%">{{ __('ESPECIAL POR CATEGORIA') }}
+                                    </h1>
                                 </div>
                             </div>
-                        </div>
-                        <div class="float-right">
-                            <a class="btn btn-primary" href="{{ route('eventos-especiales-por-categorias.index') }}"> {{ __('Back') }}</a>
                         </div>
                     </div>
 
                     <div class="card-body">
-                        
-                        <div class="form-group">
-                            <strong>Nombre:</strong>
-                            {{ $eventosEspecialesPorCategoria->nombre }}
-                        </div>
-                        <div class="form-group">
-                            <strong>Estado:</strong>
-                            {{ $eventosEspecialesPorCategoria->estado->nombre }}
-                        </div>
-                        <div class="form-group">
-                            <strong>Evento Especial:</strong>
-                            {{ $eventosEspecialesPorCategoria->categoriasEventosEspeciale->nombre }}
+
+
+                        <div class="table-responsive"
+                            style="background-color: #DEE2E6; border-radius: 18px; border-style: solid; border-width:2px; border-color: #DEE2E6">
+                            <table class="table table-bordered table-hover">
+                                <thead class="thead-dark">
+                                    <tr style="border-width: 2px">
+                                        <th>Nombre</th>
+                                        <th>Estado</th>
+                                        <th>Evento Especial</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>{{ $eventosEspecialesPorCategoria->nombre }}</td>
+                                        <td>{{ $eventosEspecialesPorCategoria->estado->nombre }}</td>
+                                        <td>{{ $eventosEspecialesPorCategoria->categoriasEventosEspeciale->nombre }}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </div>
 
                     </div>
                 </div>
             </div>
+            <div>
+                <a href="{{ route('eventos-especiales-por-categorias.index') }}" class="btn btn-outline"
+                    style="color:#00324D; border:2px solid #82DEF0; height: 40px; width:130px; cursor: pointer; border-radius: 35px; margin-top:10px; justify-content: center; justify-items: center; margin-left: 90%;"
+                    onmouseover="this.style.backgroundColor='#b2ebf2';" onmouseout="this.style.backgroundColor='#FFFF';">
+                    {{ __('REGRESAR') }}
+                    <i class="fa-solid fa-circle-play fa-flip-both" style="color: #642c78;"></i>
+                </a>
+            </div>
         </div>
+
     </section>
 @endsection
