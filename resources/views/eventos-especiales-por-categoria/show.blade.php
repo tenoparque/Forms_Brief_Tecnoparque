b8b2d9813cdf902e98ed0d5f8d50af778e233090
Darienpes29 <darienpesca29@gmail.com>
2024-03-11 10:58:22 -0500

    Edición margin-bottom y margin-top del botón y texto 'Detalle de eventos especiales'

--- resources/views/eventos-especiales-por-categoria/show.blade.php
+++ resources/views/eventos-especiales-por-categoria/show.blade.php
@@ -11,7 +11,7 @@
                 <div class="col-sm-12">
                     <div class="card-header">
                         <div class="float-left">
-                            <div class="d-flex mt-3 mb-2">
+                            <div class="d-flex mt-3 mb-4">
                                 <div>
                                     <h1 class="primeraPalabraFlex" style="font-size:200%">{{ __('DETALLE DEL EVENTO') }}</h1>
                                 </div>
@@ -51,7 +51,7 @@
             </div>
             <div>
                 <a href="{{ route('eventos-especiales-por-categorias.index') }}" class="btn btn-outline"
-                    style="color:#00324D; border:2px solid #82DEF0; height: 40px; width:130px; cursor: pointer; border-radius: 35px; margin-top:10px; justify-content: center; justify-items: center; margin-left: 90%;"
+                    style="color:#00324D; border:2px solid #82DEF0; height: 40px; width:130px; cursor: pointer; border-radius: 35px; margin-top:18px; justify-content: center; justify-items: center; margin-left: 90%;"
                     onmouseover="this.style.backgroundColor='#b2ebf2';" onmouseout="this.style.backgroundColor='#FFFF';">
                     {{ __('REGRESAR') }}
                     <i class="fa-solid fa-circle-play fa-flip-both" style="color: #642c78;"></i>
