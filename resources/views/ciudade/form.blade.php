cae69a7096a3411c8717aacb9d5263089ffb1642
Darienpes29 <darienpesca29@gmail.com>
2024-03-11 09:53:59 -0500

    Se modició la vista de 'Crear Ciudad'

--- resources/views/ciudade/form.blade.php
+++ resources/views/ciudade/form.blade.php
@@ -2,16 +2,16 @@
     <div class="box-body">
         
         <div class="form-group">
-            {{ Form::label('nombre') }}
+            {{ Form::label('nombre', null, ['style' => 'font-size: 18px; font-weight: bold']) }}
             {{ Form::text('nombre', $ciudade->nombre, ['class' => 'form-control' . ($errors->has('nombre') ? ' is-invalid' : ''), 'placeholder' => 'Nombre',
              'style' => 'width: 85%; border-radius: 50px; border-style: solid; border-width:4px; border-color: #DEE2E6; margin-bottom: 10px;']) }}
             {!! $errors->first('nombre', '<div class="invalid-feedback">:message</div>') !!}
         </div>
         @if(Route::currentRouteName() === 'ciudades.create')
             <div class="form-group">
-                <label for="id_departamento">Departamento</label>
+                <label style="font-size: 18px; font-weight: bold" for="id_departamento">Departamento</label>
                 <select name="id_departamento" id="id_departamento" class="form-control selectpicker"
-                data-style="btn-primary" title="Seleccionar Departamento" required style="width: 85%; border-radius: 50px; border-style: solid; border-width:4px; border-color: #DEE2E6; margin-bottom: 10px;">
+                data-style="btn-primary" title="Seleccionar Departamento" required >
                     @foreach ($departamentos as $departamento)
                     <!-- We go through the models of the Departamentos that we previously passed through the controller -->
                         <option value="{{ $departamento->id }}">{{ $departamento-> nombre }}</option> <!-- We obtain the id and the value -->
@@ -37,7 +37,7 @@
     </div>
     <div class="box-footer mt20" style="text-align: right;">
         <button type="submit" class="btn btn-outline"
-            style="color:#00324D; border:2px solid #82DEF0; height: 40px; width:120px; cursor: pointer; border-radius: 35px; margin-top:10px; justify-content: center; justify-items: center; margin-left: 0;" 
+            style="color:#00324D; border:2px solid #82DEF0; height: 40px; width:120px; cursor: pointer; border-radius: 35px; margin-top:15px; justify-content: center; justify-items: center; margin-left: 0;" 
             onmouseover="this.style.backgroundColor='#b2ebf2';"
             onmouseout="this.style.backgroundColor='#FFFF';">
             {{ __('GUARDAR') }}
