7030ab12ded1115830990582a33b85681cd813b7
Darienpes29 <darienpesca29@gmail.com>
2024-03-11 14:52:38 -0500

    Se modificaron todas las vistas del crud de 'Tipos de solicitudes'

--- resources/views/tipos-de-solicitude/edit.blade.php
+++ resources/views/tipos-de-solicitude/edit.blade.php
@@ -6,20 +6,20 @@
 
 @section('content')
 
-    <section class="content container mt-5">
+    <section class="container shadow p-4  my-5 bg-light rounded">
         <div class="">
             <div class="col-md-12">
 
                 @includeif('partials.errors')
 
-                <div class="card card-default">
-                    <div class="card-header">
+                <div class="">
+                    <div class="">
                         <div class="d-flex mt-3 mb-4">
                             <div>
-                                <h1 class="primeraPalabraFlex" style="font-size: 180%">{{ __('EDITAR TIPO') }}</h1>
+                                <h1 class="primeraPalabraFlex" style="font-size: 200%">{{ __('EDITAR TIPO') }}</h1>
                             </div>
                             <div>
-                                <h1 class="segundaPalabraFlex" style="font-size: 180%">{{ __('DE SOLICITUDES') }}</h1>
+                                <h1 class="segundaPalabraFlex" style="font-size: 200%">{{ __('DE SOLICITUDES') }}</h1>
                             </div>
                         </div>
                     </div>
