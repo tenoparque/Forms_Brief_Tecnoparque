e11c1cda359a3b73de3a8eb1ba3052a343645c36
Darienpes29 <darienpesca29@gmail.com>
2024-03-12 10:10:21 -0500

    Se modificó la vista 'Detalle y Editar departamento'

--- resources/views/departamento/show.blade.php
+++ resources/views/departamento/show.blade.php
@@ -5,15 +5,16 @@
 @endsection
 
 @section('content')
+
     <section class="container shadow p-4 my-5 bg-light rounded">
         <div class="container">
-            <div class="row">
-                <div class="col-sm-12">
+            <div class="col-sm-12">
+                <div class="">
                     <div class="card-header">
                         <div class="float-left">
-                            <div class="d-flex mt-3 mb-4">
+                            <div class="d-flex mt-3 mb-2">
                                 <div>
-                                    <h1 class="primeraPalabraFlex" style="font-size:200%">{{ __('EDITAR') }}</h1>
+                                    <h1 class="primeraPalabraFlex" style="font-size:200%">{{ __('DETALLE DEL') }}</h1>
                                 </div>
                                 <div>
                                     <h1 class="segundaPalabraFlex" style="font-size: 200%">{{ __('DEPARTAMENTO') }}</h1>
@@ -23,12 +24,30 @@
                     </div>
 
                     <div class="card-body">
-                        <div class="form-group">
-                            {{ Form::label('nombre', 'Nombre', ['class' => 'font-weight-bold', 'style' => 'font-size: 18px;']) }}
-                            {{ Form::text('nombre', $departamento->nombre, ['class' => 'form-control' . ($errors->has('nombre') ? ' is-invalid' : ''), 'placeholder' => 'Nombre', 'style' => 'width: 85%; border-radius: 50px; border-style: solid; border-width:4px; border-color: #DEE2E6; margin-bottom: 10px;']) }}
-                            {!! $errors->first('nombre', '<div class="invalid-feedback">:message</div>') !!}
+                        <div class="row mb-3">
                         </div>
+                        <div class="table-responsive"
+                            style="background-color: #DEE2E6; border-radius: 18px; border-style: solid; border-width:2px; border-color: #DEE2E6">
+                            <table class="table table-bordered table-hover">
+                                <thead class="thead-dark">
+                                    <tr style="border-width: 2px">
+                                        <th>Nombre</th>
+                                    </tr>
+                                </thead>
+                                <tbody class="alldata">
+                                    <tr>
+                                        <td>{{ $departamento->nombre }}</td>
+                                    </tr>
+                                </tbody>
+                                <!-- Another tbody is created for the search records -->
+                                <tbody id="Content" class="dataSearched">
+
+                                </tbody>
+                            </table>
+                        </div>
+
                     </div>
+
                 </div>
             </div>
             <div>
