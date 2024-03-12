9c58bac2de3dd9d7816c9e90b61deeaf266b84d8
deicyjejen21 <deicyjejen21@gmail.com>
2024-03-12 17:21:29 -0500

    dpeartamento text field

--- resources/views/departamento/form.blade.php
+++ resources/views/departamento/form.blade.php
@@ -1,8 +1,8 @@
 <div class="box box-info padding-1">
     <div class="box-body">
         <div class="form-group">
-            {{ Form::label('nombre', null, ['style' => 'font-size: 18px; font-weight: bold']) }}
-            {{ Form::text('nombre', $departamento->nombre, ['class' => 'form-control' . ($errors->has('nombre') ? ' is-invalid' : ''), 'placeholder' => 'Nombre','style' => 'width: 85%; border-radius: 50px; border-style: solid; border-width:4px; border-color: #DEE2E6; margin-bottom: 10px;']) }}
+            {{ Form::label('nombre', null, ['style' => 'font-size: 18px; font-weight: bold; margin-bottom:10px']) }}
+            {{ Form::text('nombre', $departamento->nombre, ['class' => 'form-control' . ($errors->has('nombre') ? ' is-invalid' : ''), 'placeholder' => 'Nombre','style' => 'width: 85%; border-radius: 50px; border-style: solid; border-width:4px; border-color: #ececec; background-color:  #ececec;height:45px; margin-bottom: 10px;']) }}
             {!! $errors->first('nombre', '<div class="invalid-feedback">:message</div>') !!}
         </div>
     </div>
