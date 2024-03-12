ebd1384d2b3c93d73710ec8146060623e387a7a6
Darienpes29 <darienpesca29@gmail.com>
2024-03-12 09:02:25 -0500

    SE MODIFICÓ LA VISTA 'EDITAR DATO ÚNICO X SOLICITUD'

--- resources/views/datos-unicos-por-solicitude/show.blade.php
+++ resources/views/datos-unicos-por-solicitude/show.blade.php
@@ -5,26 +5,27 @@
 @endsection
 
 @section('content')
-    <section class="container shadow bg-light  rounded p-4 my-5">
-        <div class="row">
-            <div class="col-md-12">
+    <section class="container shadow p-4 my-5 bg-light rounded">
+        <div class="container">
+            <div class="col-sm-12">
                 <div class="">
-                    <div class="">
+                    <div class="card-header">
                         <div class="float-left">
-                            <div class="d-flex mt-3 mb-4">
+                            <div class="d-flex mt-3 mb-2">
                                 <div>
-                                    <h1 class="primeraPalabraFlex" style="font-size: 180%">{{ __('DETALLE DE') }}</h1>
+                                    <h1 class="primeraPalabraFlex" style="font-size: 200%">{{ __('DETALLE DEL') }}</h1>
                                 </div>
                                 <div>
-                                    <h1 class="segundaPalabraFlex" style="font-size: 180%">
-                                        {{ __('DATOS UNICOS POR SOLICITUD') }}</h1>
+                                    <h1 class="segundaPalabraFlex" style="font-size: 200%">
+                                        {{ __('DATO ÚNICO POR SOLICITUD') }}</h1>
                                 </div>
                             </div>
                         </div>
-
                     </div>
 
-                    <div class="">
+                    <div class="card-body">
+                        <div class="row mb-3">
+                        </div>
                         <div class="table-responsive"
                             style="background-color: #DEE2E6; border-radius: 18px; border-style: solid; border-width:2px; border-color: #DEE2E6">
                             <table class="table table-bordered table-hover">
@@ -46,18 +47,16 @@
                                 </tbody>
                                 <!-- Another tbody is created for the search records -->
                                 <tbody id="Content" class="dataSearched">
-
                                 </tbody>
                             </table>
                         </div>
-
-
                     </div>
+
                 </div>
             </div>
             <div class="float-right">
                 <a href="{{ route('datos-unicos-por-solicitudes.index') }}" class="btn btn-outline"
-                    style="color:#00324D; border:2px solid #82DEF0; height: 40px; width:130px; cursor: pointer; border-radius: 35px; margin-top:10px; justify-content: center; justify-items: center; margin-left: 90%;"
+                    style="color:#00324D; border:2px solid #82DEF0; height: 40px; width:130px; cursor: pointer; border-radius: 35px; margin-top:18px; justify-content: center; justify-items: center; margin-left: 90%;"
                     onmouseover="this.style.backgroundColor='#b2ebf2';" onmouseout="this.style.backgroundColor='#FFFF';">
                     {{ __('REGRESAR') }}
                     <i class="fa-solid fa-circle-play fa-flip-both" style="color: #642c78;"></i>
