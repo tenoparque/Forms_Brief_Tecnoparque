fc6225507f1dbf5ace3a13bcdd0f562dae0d3115
Darienpes29 <darienpesca29@gmail.com>
2024-03-13 14:18:56 -0500

    Se editó la vista 'Crear Tipo de solicitud'

--- resources/views/role/form.blade.php
+++ resources/views/role/form.blade.php
@@ -8,10 +8,13 @@
         </div>
 
     </div>
-    <div class="box-footer mt20">
-        <button type="submit" class="btn btn-outline"  href="{{ route('roles.create') }}" class="btn btn-outline"
-        style="color:#00324D; border:2px solid #82DEF0; height: 40px; width:120px; cursor: pointer; margin-left: 90%; border-radius: 35px; margin-top:15px; justify-content: center; justify-items: center; " 
-        onmouseover="this.style.backgroundColor='#b2ebf2';" onmouseout="this.style.backgroundColor='#FFFF';">{{ __('GUARDAR') }}
-        <i class="fa-solid fa-circle-plus fa-sm" style="color: #642c78;"></i></button>
+    <div class="box-footer mt20" style="text-align: right;">
+        <button type="submit" class="btn btn-outline"
+            style="color:#00324D; border:2px solid #82DEF0; height: 40px; width:120px; cursor: pointer; border-radius: 35px; margin-top:15px; justify-content: center; justify-items: center; margin-right: 40px;" 
+            onmouseover="this.style.backgroundColor='#b2ebf2';"
+            onmouseout="this.style.backgroundColor='#FFFF';">
+            {{ __('GUARDAR') }}
+            <i class="fa-solid fa-circle-plus fa-sm" style="color: #642c78;"></i>
+        </button>
     </div>
 </div>
