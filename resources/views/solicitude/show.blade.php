c1afcea9076d0da64c1653249f0ded8363cb6370
diegoGar05 <dg8903614@gmail.com>
2024-03-13 08:42:16 -0500

    en el show solicitudes se muestra el nombre de sus foranes en vez de la id

--- resources/views/solicitude/show.blade.php
+++ resources/views/solicitude/show.blade.php
@@ -22,7 +22,7 @@
                         
                         <div class="form-group">
                             <strong>Id Tipos De Solicitudes:</strong>
-                            {{ $solicitude->id_tipos_de_solicitudes }}
+                            {{ $solicitude->tiposdesolicitude->nombre }}
                         </div>
                         <div class="form-group">
                             <strong>Fecha Y Hora De La Solicitud:</strong>
@@ -30,15 +30,15 @@
                         </div>
                         <div class="form-group">
                             <strong>Id Usuario Que Realiza La Solicitud:</strong>
-                            {{ $solicitude->id_usuario_que_realiza_la_solicitud }}
+                            {{ $solicitude->user->name }}
                         </div>
                         <div class="form-group">
                             <strong>Id Eventos Especiales Por Categorias:</strong>
-                            {{ $solicitude->id_eventos_especiales_por_categorias }}
+                            {{ $solicitude->eventosespecialesporcategoria->nombre }}
                         </div>
                         <div class="form-group">
                             <strong>Id Estado De La Solicitud:</strong>
-                            {{ $solicitude->id_estado_de_la_solicitud }}
+                            {{ $solicitude->estadosDeLasSolictude->nombre }}
                         </div>
 
                         <div class="form-group">
