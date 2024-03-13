2c4690fb05b5ec4e54ebe6690583035bfc44a76a
Darienpes29 <darienpesca29@gmail.com>
2024-03-13 15:33:30 -0500

    Se modificó la vista 'Editar Tipo de solicitud'

--- resources/views/tipos-de-solicitude/form.blade.php
+++ resources/views/tipos-de-solicitude/form.blade.php
@@ -1,23 +1,31 @@
 <div class="box box-info padding-1">
     <div class="box-body">
-
         <div class="form-group">
             {{ Form::label('nombre', null, ['style' => 'font-size: 18px; font-weight: bold; margin-left: 35px']) }}
-            {{ Form::text('nombre', $tiposDeSolicitude->nombre, ['class' => 'form-control' . ($errors->has('nombre') ? ' is-invalid' : ''), 'placeholder' => 'Nombre', 'style' => 'width: 95%; border-radius: 50px; border-style: solid; border-width:4px;  border-color: #ececec; background-color:  #ececec;  margin-left: 25px; margin-bottom: 10px;']) }}
+            {{ Form::text('nombre', $tiposDeSolicitude->nombre, ['class' => 'form-control' . ($errors->has('nombre') ? ' is-invalid' : ''), 'placeholder' => 'Nombre', 'style' => 'width: 95%; border-radius: 50px; border-style: solid; border-width:4px;  border-color: #ececec; background-color:  #ececec; margin-bottom: 10px; margin-left: 25px;']) }}
             {!! $errors->first('nombre', '<div class="invalid-feedback">:message</div>') !!}
         </div>
         @if (Route::currentRouteName() === 'tipos-de-solicitudes.edit')
             <div class="form-group">
-                <label for="id_estado" style="font-size: 18px; font-weight: bold">Estado</label>
-                <select name="id_estado" id="id_estado" class="form-control selectpicker" data-style="btn-primary"
-                    title="Seleccionar Estado" required>
-                    @foreach ($estados as $estado)
-                        <option value="{{ $estado->id }}"
-                            {{ ($tiposDeSolicitude->id_estado ?? '') == $estado->id ? 'selected' : '' }}>
-                            {{ $estado->nombre }}
-                        </option>
-                    @endforeach
-                </select>
+                <label for="id_estado" style="font-size: 18px; font-weight: bold; margin-left: 35px">Estado</label>
+                <div style="position: relative;">
+                    <select name="id_estado" id="id_estado" class="form-control selectpicker" data-style="btn-primary"
+                        title="Seleccionar Estado" required
+                        style="width: 95%; height:45px; border-radius: 50px; border-color: #ececec; background-color:  #ececec; margin-bottom: 10px; margin-top:8px; margin-left: 25px;padding-right: 30px; -webkit-appearance: none; -moz-appearance: none; appearance: none;">
+                        @foreach ($estados as $estado)
+                            <option value="{{ $estado->id }}"
+                                {{ ($tiposDeSolicitude->id_estado ?? '') == $estado->id ? 'selected' : '' }}>
+                                {{ $estado->nombre }}
+                            </option>
+                        @endforeach
+                    </select>
+                    <div class="icono" style="right: 4%">
+                        <div class="circle-play">
+                            <div class="circle"></div>
+                            <div class="triangle"></div>
+                        </div>
+                    </div>
+                </div>
             </div>
         @endif
     </div>
