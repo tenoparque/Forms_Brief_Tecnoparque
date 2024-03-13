e43060b52e3700ddf2ee6685515672917ec4d6ce
deicyjejen21 <deicyjejen21@gmail.com>
2024-03-13 11:30:41 -0500

    modificacion personalizaciones

--- resources/views/personalizacione/form.blade.php
+++ resources/views/personalizacione/form.blade.php
@@ -1,84 +1,107 @@
 <div class="container">
+    
     <div class="row">
-        
-        <div class="col-md-6" style="margin-bottom: 15px">
-            {{ Form::label('logo', 'Seleccionar imagen') }}
-            <input type="file" name="logo" id="logo" class="form-control-file{{ $errors->has('logo') ? ' is-invalid' : '' }}">
-            {!! $errors->first('logo', '<div class="invalid-feedback">:message</div>') !!}
-        </div>
-        @if(Route::currentRouteName() === 'personalizaciones.create')
-        <div id="imagePreview">
-            <img id="logoImage" class="img-thumbnail" alt="Preview" style="max-width: 300px; max-height: 300px;">
-        </div>
-        @endif
-        @if(Route::currentRouteName() === 'personalizaciones.edit')
-        <div class="form-group">
-            <label for="logo" style="font-size: 18px; font-weight: bold; margin-bottom: 15px">Imagen QR:</label>
-            <!-- Agrega la etiqueta img con el ID 'qrImage' -->
-            <img id="logoImage" src="{{ $personalizacione->logo ? 'data:image/png;base64,' . base64_encode($personalizacione->logo) : '' }}" alt="LOGO" width="200">
-        </div>
-        @endif
-        <!-- <div class="col-md-6">
-            {{ Form::label('color_principal',null, ['style' => 'font-size: 18px; font-weight: bold']) }}
-            {{ Form::text('color_principal', $personalizacione->color_principal, ['class' => 'form-control' . ($errors->has('color_principal') ? ' is-invalid' : ''), 'placeholder' => 'Color Principal','style' => 'width: 85%; border-radius: 50px; border-style: solid; border-width:4px; border-color: #DEE2E6; margin-bottom: 10px;']) }}
+        <div class="d-flex" style="margin-top: 2%; margin-bottom: 2%">
+
+            <div class="col-md-6" style="margin-bottom: 15px">
+                {{ Form::label('logo', 'Seleccionar imagen') }}
+                <input style="margin-bottom: 2%;" type="file" name="logo" id="logo"
+                    class="form-control-file{{ $errors->has('logo') ? ' is-invalid' : '' }}">
+                {!! $errors->first('logo', '<div class="invalid-feedback">:message</div>') !!}
+            </div>
+            @if (Route::currentRouteName() === 'personalizaciones.create')
+                <div id="imagePreview">
+                    <img id="logoImage" class="img-thumbnail" alt="Preview"
+                        style="max-width: 300px; max-height: 300px;">
+                </div>
+            @endif
+            @if (Route::currentRouteName() === 'personalizaciones.edit')
+                <div class="col-md-6" style="margin-bottom: 15px;">
+                    <label for="logo" style="font-size: 18px; font-weight: bold; margin-bottom: 15;margin-right: 2%;">Logo </label>
+                    <!-- Agrega la etiqueta img con el ID 'qrImage' -->
+                    <img id="logoImage"
+                        src="{{ $personalizacione->logo ? 'data:image/png;base64,' . base64_encode($personalizacione->logo) : '' }}"
+                        alt="LOGO" width="200">
+                </div>
+            @endif
+            <!-- <div class="col-md-6">
+            {{ Form::label('color_principal', null, ['style' => 'font-size: 18px; font-weight: bold']) }}
+            {{ Form::text('color_principal', $personalizacione->color_principal, ['class' => 'form-control' . ($errors->has('color_principal') ? ' is-invalid' : ''), 'placeholder' => 'Color Principal', 'style' => 'width: 85%; border-radius: 50px; border-style: solid; border-width:4px; border-color: #DEE2E6; margin-bottom: 10px;']) }}
             {!! $errors->first('color_principal', '<div class="invalid-feedback">:message</div>') !!}
-        </div> -->
+            </div> -->
+        </div>
+        <div class="d-flex" style="margin-top: 2%; margin-bottom: 2%">
+            <label style="font-size: 18px; font-weight: bold; margin-left: 35px;margin-right: 15px; ">Color
+                principal</label>
+            <input id="color-picker-principal" name="color_principal"
+                value="{{ $personalizacione->color_principal }}" />
 
-        <!-- Input para el color picker -->
-        <label>Color principal</label>
-        <input id="color-picker-principal" name="color_principal" value="{{ $personalizacione->color_principal }}" />
+            <!-- Input para el color picker -->
+            <label style="font-size: 18px; font-weight: bold; margin-left: 35px;margin-right: 15px;">Color
+                Secundario</label>
+            <input id="color-picker-secundario" name="color_secundario"
+                value="{{ $personalizacione->color_secundario }}" />
 
-        <!-- Input para el color picker -->
-        <label>Color Secundario</label>
-        <input id="color-picker-secundario" name="color_secundario" value="{{ $personalizacione->color_secundario }}" />
+            <!-- Input para el color picker -->
+            <label style="font-size: 18px; font-weight: bold; margin-right: 15px; margin-left: 35px;">Color
+                Terciario</label>
+            <input id="color-picker-terciario" name="color_terciario"
+                value="{{ $personalizacione->color_terciario }}" />
 
-        <!-- Input para el color picker -->
-        <label>Color Terciario</label>
-        <input id="color-picker-terciario" name="color_terciario" value="{{ $personalizacione->color_terciario }}" />
-
-
+        </div>
         <!-- <div class="col-md-6">
-            {{ Form::label('color_secundario',null, ['style' => 'font-size: 18px; font-weight: bold']) }}
-            {{ Form::text('color_secundario', $personalizacione->color_secundario, ['class' => 'form-control' . ($errors->has('color_secundario') ? ' is-invalid' : ''), 'placeholder' => 'Color Secundario','style' => 'width: 85%; border-radius: 50px; border-style: solid; border-width:4px; border-color: #DEE2E6; margin-bottom: 10px;']) }}
+            {{ Form::label('color_secundario', null, ['style' => 'font-size: 18px; font-weight: bold']) }}
+            {{ Form::text('color_secundario', $personalizacione->color_secundario, ['class' => 'form-control' . ($errors->has('color_secundario') ? ' is-invalid' : ''), 'placeholder' => 'Color Secundario', 'style' => 'width: 85%; border-radius: 50px; border-style: solid; border-width:4px; border-color: #DEE2E6; margin-bottom: 10px;']) }}
             {!! $errors->first('color_secundario', '<div class="invalid-feedback">:message</div>') !!}
         </div>
         <div class="col-md-6">
-            {{ Form::label('color_terciario',null, ['style' => 'font-size: 18px; font-weight: bold']) }}
-            {{ Form::text('color_terciario', $personalizacione->color_terciario, ['class' => 'form-control' . ($errors->has('color_terciario') ? ' is-invalid' : ''), 'placeholder' => 'Color Terciario','style' => 'width: 85%; border-radius: 50px; border-style: solid; border-width:4px; border-color: #DEE2E6; margin-bottom: 10px;']) }}
+            {{ Form::label('color_terciario', null, ['style' => 'font-size: 18px; font-weight: bold']) }}
+            {{ Form::text('color_terciario', $personalizacione->color_terciario, ['class' => 'form-control' . ($errors->has('color_terciario') ? ' is-invalid' : ''), 'placeholder' => 'Color Terciario', 'style' => 'width: 85%; border-radius: 50px; border-style: solid; border-width:4px; border-color: #DEE2E6; margin-bottom: 10px;']) }}
             {!! $errors->first('color_terciario', '<div class="invalid-feedback">:message</div>') !!}
         </div> -->
-        @if(Route::currentRouteName() === 'personalizaciones.edit')
-        <div class="col-md-6">
-        <label for="id_estado" style="font-size: 18px; font-weight: bold">Estado</label>
-        <select name="id_estado" id="id_estado" class="form-control selectpicker"
-        data-style="btn-primary" title="Seleccionar Estado" required>
-            @foreach ($estados as $estado)
-                <option value="{{ $estado->id }}" {{ ($personalizacione->id_estado ?? '') == $estado->id ? 'selected' : '' }}>
-                    {{ $estado->nombre }}
-                </option>
-            @endforeach
-        </select>
-        </div>
+        @if (Route::currentRouteName() === 'personalizaciones.edit')
+            <div class="col-md-6">
+                <label for="id_estado" style="font-size: 18px; font-weight: bold; margin-left: 35px;">Estado</label>
+                <div style="position: relative;">
+                    <select
+                        style="width: 95%; height:45px; border-radius: 50px; border-color: #ececec; background-color:  #ececec; margin-bottom: 10px; margin-top:8px; margin-left: 25px;padding-right: 30px; -webkit-appearance: none; -moz-appearance: none; appearance: none;"
+                        name="id_estado" id="id_estado" class="form-control selectpicker" data-style="btn-primary"
+                        title="Seleccionar Estado" required>
+                        @foreach ($estados as $estado)
+                            <option value="{{ $estado->id }}"
+                                {{ ($personalizacione->id_estado ?? '') == $estado->id ? 'selected' : '' }}>
+                                {{ $estado->nombre }}
+                            </option>
+                        @endforeach
+                    </select>
+                    <div class="icono" style="right: 4%">
+                        <div class="circle-play">
+                            <div class="circle"></div>
+                            <div class="triangle"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         @endif
 
 
-        
+
         <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            var logoInput = document.getElementById('logo');
-            var logoImage = document.getElementById('logoImage');
+            document.addEventListener('DOMContentLoaded', function() {
+                var logoInput = document.getElementById('logo');
+                var logoImage = document.getElementById('logoImage');
 
-            logoInput.addEventListener('change', function(event) {
-                var file = event.target.files[0];
-                var reader = new FileReader();
+                logoInput.addEventListener('change', function(event) {
+                    var file = event.target.files[0];
+                    var reader = new FileReader();
 
-                reader.onload = function(e) {
-                    logoImage.src = e.target.result;
-                };
+                    reader.onload = function(e) {
+                        logoImage.src = e.target.result;
+                    };
 
-                reader.readAsDataURL(file);
+                    reader.readAsDataURL(file);
+                });
             });
-        });
         </script>
 
         <script type="text/javascript">
@@ -114,29 +137,30 @@
 
 
         <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            var logoInput = document.getElementById('logo');
-            var logoImage = document.getElementById('logoImage');
+            document.addEventListener('DOMContentLoaded', function() {
+                var logoInput = document.getElementById('logo');
+                var logoImage = document.getElementById('logoImage');
 
-            logoInput.addEventListener('change', function(event) {
-                var file = event.target.files[0];
-                var reader = new FileReader();
+                logoInput.addEventListener('change', function(event) {
+                    var file = event.target.files[0];
+                    var reader = new FileReader();
 
-                reader.onload = function(e) {
-                    logoImage.src = e.target.result;
-                };
+                    reader.onload = function(e) {
+                        logoImage.src = e.target.result;
+                    };
 
-                reader.readAsDataURL(file);
+                    reader.readAsDataURL(file);
+                });
             });
-        });
         </script>
 
     </div>
     <div class="box-footer mt20">
-        <button type="submit" class="btn btn-outline"  href="{{ route('personalizaciones.create') }}" class="btn btn-outline"
-        style="color:#00324D; border:2px solid #82DEF0; height: 40px; width:120px; cursor: pointer; margin-left: 90%; border-radius: 35px; margin-top:15px; justify-content: center; justify-items: center; " 
-        onmouseover="this.style.backgroundColor='#b2ebf2';" onmouseout="this.style.backgroundColor='#FFFF';">{{ __('GUARDAR') }}
-        <i class="fa-solid fa-circle-plus fa-sm" style="color: #642c78;"></i></button>
+        <button type="submit" class="btn btn-outline" href="{{ route('personalizaciones.create') }}"
+            class="btn btn-outline"
+            style="color:#00324D; border:2px solid #82DEF0; height: 40px; width:120px; cursor: pointer; margin-left: 90%; border-radius: 35px; margin-top:15px; justify-content: center; justify-items: center; "
+            onmouseover="this.style.backgroundColor='#b2ebf2';"
+            onmouseout="this.style.backgroundColor='#FFFF';">{{ __('GUARDAR') }}
+            <i class="fa-solid fa-circle-plus fa-sm" style="color: #642c78;"></i></button>
     </div>
 </div>
-
