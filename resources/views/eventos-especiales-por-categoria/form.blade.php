a36472f9df6baf819faa7d4d87f7040f20100d97
deicyjejen21 <deicyjejen21@gmail.com>
2024-03-14 10:41:37 -0500

    edit nodos

--- resources/views/eventos-especiales-por-categoria/form.blade.php
+++ resources/views/eventos-especiales-por-categoria/form.blade.php
@@ -14,7 +14,7 @@
                         style="width: 95%; height:45px; border-radius: 50px; border-color: #ececec; background-color:  #ececec; margin-bottom: 10px; margin-top:8px; margin-left: 25px;padding-right: 30px; -webkit-appearance: none; -moz-appearance: none; appearance: none;"
                         name="id_estado" id="id_estado" class="form-control selectpicker" data-style="btn-primary"
                         title="Seleccionar Estado" required ">
-                      @foreach ($estados as $estado)
+                       @foreach ($estados as $estado)
                         <!-- We go through the models of the estados that we previously passed through the controller -->
                         <option value="{{ $estado->id }}"
                             {{ ($eventosEspecialesPorCategoria->id_estado ?? '') == $estado->id ? 'selected' : '' }}>
@@ -32,46 +32,54 @@
 </div>
 @endif
 @if (Route::currentRouteName() === 'eventos-especiales-por-categorias.create')
-<div class="form-group">
-    <label style="font-size: 18px; font-weight: bold; margin-left: 35px;" for="id_eventos_especiales">Categoria</label>
-    <div style="position: relative;">
-        <select style="width: 95%; height:45px; border-radius: 50px; border-color: #ececec; background-color:  #ececec; margin-bottom: 10px; margin-top:8px; margin-left: 25px;padding-right: 30px; -webkit-appearance: none; -moz-appearance: none; appearance: none;" name="id_eventos_especiales" id="id_eventos_especiales" class="form-control selectpicker" data-style="btn-primary" title="Seleccionar Categoria" required ">
+    <div class="form-group">
+        <label style="font-size: 18px; font-weight: bold; margin-left: 35px;"
+            for="id_eventos_especiales">Categoria</label>
+        <div style="position: relative;">
+            <select
+                style="width: 95%; height:45px; border-radius: 50px; border-color: #ececec; background-color:  #ececec; margin-bottom: 10px; margin-top:8px; margin-left: 25px;padding-right: 30px; -webkit-appearance: none; -moz-appearance: none; appearance: none;"
+                name="id_eventos_especiales" id="id_eventos_especiales" class="form-control selectpicker"
+                data-style="btn-primary" title="Seleccionar Categoria" required ">
             <option value="" disabled selected>Seleccionar categoría...</option>
-            @foreach ($categorias as $categoria)
+             @foreach ($categorias as $categoria)
                 <!-- We go through the models of the categorias that we previously passed through the controller -->
                 <option value="{{ $categoria->id }}">{{ $categoria->nombre }}</option>
                 <!-- We obtain the id and the value -->
-            @endforeach
-        </select>
-        <div class="icono" style="right: 4%">
-            <div class="circle-play">
-                <div class="circle"></div>
-                <div class="triangle"></div>
-            </div>
-        </div>
+@endforeach
+</select>
+<div class="icono" style="right: 4%">
+    <div class="circle-play">
+        <div class="circle"></div>
+        <div class="triangle"></div>
     </div>
+</div>
+</div>
 </div>
 
 @endif
 @if (Route::currentRouteName() === 'eventos-especiales-por-categorias.edit')
-   <div class="form-group">
-    <label style="font-size: 18px; font-weight: bold; margin-left: 35px;" for="id_eventos_especiales">Categoria</label>
-    <div style="position: relative;">
-        <select style="width: 95%; height:45px; border-radius: 50px; border-color: #ececec; background-color:  #ececec; margin-bottom: 10px; margin-top:8px; margin-left: 25px;padding-right: 30px; -webkit-appearance: none; -moz-appearance: none; appearance: none;" name="id_eventos_especiales" id="id_eventos_especiales" class="form-control selectpicker" data-style="btn-primary" title="Seleccionar Categoria" required ">
+    <div class="form-group">
+        <label style="font-size: 18px; font-weight: bold; margin-left: 35px;"
+            for="id_eventos_especiales">Categoria</label>
+        <div style="position: relative;">
+            <select
+                style="width: 95%; height:45px; border-radius: 50px; border-color: #ececec; background-color:  #ececec; margin-bottom: 10px; margin-top:8px; margin-left: 25px;padding-right: 30px; -webkit-appearance: none; -moz-appearance: none; appearance: none;"
+                name="id_eventos_especiales" id="id_eventos_especiales" class="form-control selectpicker"
+                data-style="btn-primary" title="Seleccionar Categoria" required ">
             <option value="" disabled selected>Seleccionar categoría...</option>
-            @foreach ($categorias as $categoria)
+             @foreach ($categorias as $categoria)
                 <!-- We go through the models of the categorias that we previously passed through the controller -->
                 <option value="{{ $categoria->id }}">{{ $categoria->nombre }}</option>
                 <!-- We obtain the id and the value -->
-            @endforeach
-        </select>
-        <div class="icono" style="right: 4%">
-            <div class="circle-play">
-                <div class="circle"></div>
-                <div class="triangle"></div>
-            </div>
-        </div>
+@endforeach
+</select>
+<div class="icono" style="right: 4%">
+    <div class="circle-play">
+        <div class="circle"></div>
+        <div class="triangle"></div>
     </div>
+</div>
+</div>
 </div>
 
 @endif
