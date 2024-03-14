b93f1b34625eb10a5725788bfd4528366e5c3aab
Darienpes29 <darienpesca29@gmail.com>
2024-03-14 11:55:16 -0500

    Modificación del Home al iniciar sesión

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,24 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+    <div class="container shadow p-4 my-5 bg-light rounded">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="card-header">
+                        <div style="d-flex justify-content-between align-items-center">
+                            <div style="d-flex justify-content-between align-items-center">
+                                <div class="d-flex mt-3 mb-4">
+                                    <div>
+                                        <h1 class="primeraPalabraFlex" style="margin-right: 0; font-size: 200%; font-weight: 900; color: rgb(0, 49, 77)"> {{ __('DASHBOARD') }}</h1>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    @endif
+                    </div>
 
-                    {{ __('You are logged in!') }}
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+                        <h4>Bienvenido a BRIEF, Gestión de Formularios Tecnoparque!</h4>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
