4b6f9748650b7d45b2a11c80244d33e5d30befab
JAAP2104 <almeidaperezjosephalexis@gmail.com>
2024-03-18 17:54:17 -0500

    de la vista editar-orden.blade.php lo modifique y ya no esta en la estructura de una tabla

--- resources/views/estados-de-las-solictude/editar-orden.blade.php
+++ resources/views/estados-de-las-solictude/editar-orden.blade.php
@@ -15,56 +15,44 @@
                     </div>
                 </div>
             </div>
-        <form id="editarOrdenForm" action="{{ route('estados-de-las-solictudes.actualizar-orden') }}" method="POST">
-            @csrf
-            @method('PUT')
-            
-                <div class="col-md-8">
-                    <div class="table-responsive"  style="background-color: #DEE2E6; border-radius: 18px; border-style: solid; border-width:2px; border-color: #DEE2E6">
+            <form id="editarOrdenForm" action="{{ route('estados-de-las-solictudes.actualizar-orden') }}" method="POST">
+                @csrf
+                @method('PUT')
 
-                        <table class="table table-bordered table-hover"
-                           >
-                            <thead class="thead-dark">
-                                <tr>
-                                    <th>Nombre</th>
-                                    <th>Orden Mostrado</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($estadosDeLasSolictudes as $estado)
-                                    <tr>
-                                        <td>{{ $estado->nombre }}</td>
-                                        <td>
-                                           <div style="position: relative">
-                                            <select name="orden_mostrado[{{ $estado->id }}]" class="orden-mostrado" required
-                                                style="width: 90%; height:45px; border-radius: 50px; border-color: #ececec; background-color:  #ececec; margin-bottom: 10px; margin-top:8px; margin-left: 25px;padding: 10px; -webkit-appearance: none; -moz-appearance: none; appearance: none;">
-                                                @for ($i = 1; $i <= count($estadosDeLasSolictudes); $i++)
-                                                    <option value="{{ $i }}"
-                                                        {{ $estado->orden_mostrado == $i ? 'selected' : '' }}>{{ $i }}
-                                                    </option>
-                                                @endfor
-                                            </select>
-                                            <div class="icono" style="right: 7%;">
-                                                <div class="circle-play">
-                                                    <div class="circle"></div>
-                                                    <div class="triangle"></div>
+                <div class="col-md-8">
+                    @foreach ($estadosDeLasSolictudes as $estado)
+                    <label style="font-size: 18px; font-weight: bold; margin-left: 35px"
+                    for="id_ciudad"> {{ $estado->nombre }}</label>
+                                      
+                                            <div style="position: relative">
+                                                <select name="orden_mostrado[{{ $estado->id }}]" class="orden-mostrado"
+                                                    required
+                                                    style="width: 90%; height:45px; border-radius: 50px; border-color: #ececec; background-color:  #ececec; margin-bottom: 10px; margin-top:8px; margin-left: 25px;padding: 10px; -webkit-appearance: none; -moz-appearance: none; appearance: none;">
+                                                    @for ($i = 1; $i <= count($estadosDeLasSolictudes); $i++)
+                                                        <option value="{{ $i }}"
+                                                            {{ $estado->orden_mostrado == $i ? 'selected' : '' }}>
+                                                            {{ $i }}
+                                                        </option>
+                                                    @endfor
+                                                </select>
+                                                <div class="icono" style="right: 7%;">
+                                                    <div class="circle-play">
+                                                        <div class="circle"></div>
+                                                        <div class="triangle"></div>
+                                                    </div>
                                                 </div>
                                             </div>
-                                           </div>
-                                        </td>
-                                    </tr>
+                                        
+                                    
                                 @endforeach
-                            </tbody>
-                        </table>
-                    </div>
                 </div>
-            </div>
+        </div>
 
-            <div class="box-footer" style="text-align: right;margin-right:3%;">
-                <button type="submit" class="btnGuardar" >Guardar
-                    <i class="fa-solid fa-circle-plus fa-sm iconDCR" ></i>
-                </button>
-            </div>
+        <div class="box-footer" style="text-align: right;margin-right:3%;">
+            <button type="submit" class="btnGuardar">Guardar
+                <i class="fa-solid fa-circle-plus fa-sm iconDCR"></i>
+            </button>
+        </div>
         </form>
     </section>
 
@@ -79,7 +67,7 @@
 
                 $('.orden-mostrado').each(function() {
                     ordenesMostrados.push($(this)
-                .val()); // Obtener el valor de cada campo de orden mostrado
+                        .val()); // Obtener el valor de cada campo de orden mostrado
                 });
 
                 var uniqueOrdenesMostrados = [...new Set(ordenesMostrados)]; // Eliminar duplicados
