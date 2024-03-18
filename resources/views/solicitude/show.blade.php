fdf7fe79a6c967f88ba535e58430c3801c123ff3
deicyjejen21 <deicyjejen21@gmail.com>
2024-03-18 15:04:46 -0500

    solicitudes

--- resources/views/solicitude/show.blade.php
+++ resources/views/solicitude/show.blade.php
@@ -39,7 +39,7 @@
                     </div>
 
                     <div class="card-body">
-                        <div class="table-responsive" style="background-color: transparent; border-color:transparent">
+                        <div class="table-responsive" style="background-color: transparent; border-color:transparent; margin-block-start: 10px;">
                             <table class="table table-bordered table-hover"
                                 style="background-color: transparent; border-color: transparent">
                                 <thead class="thead-dark">
