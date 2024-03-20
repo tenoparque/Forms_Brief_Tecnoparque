b3a686a4e5a478ea400f6167896bf87e85a92e32
JAAP2104 <almeidaperezjosephalexis@gmail.com>
2024-03-20 10:20:06 -0500

    el boton guardar de roles ya esta  funcionando nueamente de la vista form.blade.php

--- resources/views/role/form.blade.php
+++ resources/views/role/form.blade.php
@@ -1,4 +1,4 @@
-<div class="box box-info padding-1">
+{{-- <div class="box box-info padding-1">
     <div class="box-body">
         <div class="form-group">
             {{ Form::label('nombre', null, ['style' => 'font-size: 18px; font-weight: bold; margin-left: 35px;']) }}
@@ -11,5 +11,22 @@
             {{ __('GUARDAR') }}
             <i class="fa-solid fa-circle-plus fa-sm iconDCR" ></i>
         </button>
+    </div>
+</div> --}}
+
+<div class="box box-info padding-1">
+    <div class="box-body">
+        
+        <div class="form-group">
+            {{ Form::label('Nombres',null,['style' => 'font-size: 18px; font-weight: bold; margin-left: 35px;']) }}
+            {{ Form::text('name', $role->name, ['class' => 'form-control' . ($errors->has('name') ? ' is-invalid' : ''), 'placeholder' => 'Name','style' => 'width: 95%; border-radius: 50px; border-style: solid; border-width:4px; border-color: #ececec; background-color:  #ececec;height:45px; margin-bottom: 10px; margin-left: 25px;']) }}
+            {!! $errors->first('name', '<div class="invalid-feedback">:message</div>') !!}
+        </div>
+
+    </div>
+    <div class="box-footer mt20">
+        <button type="submit" class="btn btn-outline"  href="{{ route('roles.create') }}" class="btn btn-outline"
+        style="color:#00324D; border:2px solid #82DEF0; height: 40px; width:120px; cursor: pointer; margin-left: 90%; border-radius: 35px; margin-top:10px; justify-content: center; justify-items: center; ">{{ __('GUARDAR') }}
+        <i class="fa-solid fa-circle-plus fa-sm" style="color: #642c78;"></i></button>
     </div>
 </div>
