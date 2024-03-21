30cb2fb088141cda65c7eb1ebee418cbfadbc0a5
Brandon <brandon.buendia@jala.university>
2024-03-21 16:57:00 -0500

    comentario de la implementacion del command creado en el archivo kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -11,7 +11,7 @@ class Kernel extends ConsoleKernel
      * Define the application's command schedule.
      */
     protected function schedule(Schedule $schedule): void
-    {
+    {   // configure el comando previamente creado y lo configure en el kernel para que se pueda ejecutar 
         $schedule->command('suma:prueba')->everyFiveSeconds();
     }
 
