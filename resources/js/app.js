80d6fa111d2b2865538181800cdbf68295963dc8
deicyjejen21 <deicyjejen21@gmail.com>
2024-03-21 09:01:12 -0500

    fondo login

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,30 +2,32 @@ import './bootstrap';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 
-const images = [
-    'images/fondoBrief4.jpg',
-    'images/fondo2.jpg',
-    'images/fondo3.jpg',
-    // Agrega más rutas de imagen aquí
-];
-
-
-//Función para deshabilitar el Select para cambiar el estado de una solicitud
-document.getElementById('btnEditarEstado').addEventListener('click', function() {
-    document.getElementById('cboEstados').disabled = false;
-});
-
+// Define la función para mostrar una imagen de fondo aleatoria
 function displayRandomBackgroundImage() {
+    const images = [
+        'images/fondoBrief4.jpg',
+        'images/fondo2.jpg',
+        'images/fondo3.jpg',
+        // Agrega más rutas de imagen aquí
+    ];
+
     const randomImage = images[Math.floor(Math.random() * images.length)];
     document.body.style.backgroundImage = `url(${randomImage})`;
     document.body.style.backgroundRepeat = 'repeat';
 }
 
-// Verifica si estamos en la vista de inicio de sesión (por ejemplo, la URL contiene "/login")
-if (window.location.pathname.includes('/login')) {
-    displayRandomBackgroundImage(); // Mostrar una imagen de fondo aleatoria inicialmente
-    setInterval(displayRandomBackgroundImage, 1000);
+// Define la función que se ejecutará cuando el DOM esté completamente cargado
+function initialize() {
+    // Verifica si estamos en la vista de inicio de sesión
+    if (window.location.pathname.includes('/login')) {
+        displayRandomBackgroundImage(); // Muestra una imagen de fondo aleatoria inicialmente
+        setInterval(displayRandomBackgroundImage, 1000); // Cambia la imagen de fondo cada segundo
+    }
 }
+
+// Registra el evento DOMContentLoaded para ejecutar la función initialize cuando el DOM esté listo
+document.addEventListener('DOMContentLoaded', initialize);
+
 
 
 $(document).ready(function() {
