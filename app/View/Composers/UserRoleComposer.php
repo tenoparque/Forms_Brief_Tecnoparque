7d379372d9ead54210a015efa483c13009744289
JohanOvi <t0loza0viedoangelj0h4n@gmail.com>
2024-03-22 15:22:38 -0500

    Se agrega el nombre y el rol del usuario autenticado en las vistas.
    # DETALLE DEL COMMIT
    
    - Se busca el nombre en el UserRoleComposer.php y se retorna del UserRoleComposer con la variable nombreUsuario
    - En el ViewServiceProvider se obtiene esa variable nombreUsuario y se retorna a ciertas vistas especificas
    - En la vista app.blade.php se referencia la variable nombreUsuario en el <h5> donde dice BIENVENIDO
    - En la vista app.blade.php se referencia nombreRol en el <span>

--- app/View/Composers/UserRoleComposer.php
+++ app/View/Composers/UserRoleComposer.php
@@ -10,13 +10,26 @@ class UserRoleComposer
     public function compose(View $view)
     {
         if (Auth::check()) {
-            // El usuario está autenticado
-            $usuario = Auth::user();
-            $nombreRol = $usuario->roles->first()->name; // Obtén el nombre del primer rol (ajústalo según tus necesidades)
-            $view->with('nombreRol', $nombreRol);
+            $nombreRol = '';
+            $nombreUsuario = '';
+        
+            try {
+                // El usuario está autenticado
+                $usuario = Auth::user();
+                $nombreRol = $usuario->roles->first()->name; // Obtén el nombre del primer rol (ajústalo según tus necesidades)
+                $nombreUsuario = $usuario->name;
+            } catch (\Exception $e) {
+                // Manejo de la excepción
+                $nombreRol = 'Error al obtener el rol';
+                $nombreUsuario = 'Error al obtener el nombre de usuario';
+            }
+
+            $view->with('nombreRol', $nombreRol)
+                 ->with('nombreUsuario', $nombreUsuario);
         } else {
             // El usuario no está autenticado
-            $view->with('nombreRol', 'Usuario no autenticado');
+            $view->with('nombreRol', 'Usuario no autenticado')
+                 ->with('nombreUsuario', ''); // Puedes asignar un valor predeterminado aquí si lo deseas
         }
     }
 }
