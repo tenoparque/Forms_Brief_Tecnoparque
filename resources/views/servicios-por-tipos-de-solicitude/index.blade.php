33daf108602cf9d7deea8542dc905eb6461207c8
Darienpes29 <darienpesca29@gmail.com>
2024-03-22 14:40:54 -0500

    Se modificó la vista Index de Servicios  x tipo de solicitud

--- resources/views/servicios-por-tipos-de-solicitude/index.blade.php
+++ resources/views/servicios-por-tipos-de-solicitude/index.blade.php
@@ -27,10 +27,9 @@
             </div>          
             <div class="">
                 <div class="row mb-3">
-                    <p style="color: transparent">.</p>
                     <div class="col d-flex justify-content-between align-items-center">
                         
-                        <input class="form-control" id="search" placeholder="Ingrese el nombre deL nodo..." style="width: 70%; border-radius: 50px; border-style: solid; border-width:4px; border-color: #DEE2E6">
+                        <input class="form-control" id="search" placeholder="Ingrese el nombre del servicio..." style="width: 70%; border-radius: 50px; border-style: solid; border-width:4px; border-color: #DEE2E6">
 
                         <a href="{{ route('servicios-por-tipos-de-solicitudes.create') }}" class="btnCrear"
                         >{{ __('CREAR') }}
