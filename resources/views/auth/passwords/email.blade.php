3dc1e6d4f9fa1afed3a9a9eecea75bd8e2653b16
JAAP2104 <almeidaperezjosephalexis@gmail.com>
2024-04-02 22:09:28 -0500

    nuevos estilos 3.0 al email.blad.php

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -2,22 +2,13 @@
 
 @section('content')
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
-    <div class="container-fluid"
+    <div class=""
         style="background-image: url('{{ asset('images/fondoBrief4.jpg') }}'); background-position: center; min-height: 100vh; margin-block-start: -190px">
-        <div class="row  justify-content-center">
-            <div class="col-md-4 ">
-                <div class="flex-container shadow p-4  bg-light" style="margin-block-start: 200px">
+        <div class="line__email-pass">
+            <div class="flex-direction">
+                
                    
-                    <form method="POST" class="form__emailPass" action="{{ route('password.email') }}">
-                        
-                        <div class="form-img">
-                            <div class="img-container">
-                                <img src="../images/recursos/email_green.png" alt="">
-                            </div>
-                           
-                        </div>
-                        <div class="card-header  titlerestablecer">{{ __('Restablecer contraseña') }}</div>
-                        
+                    <form class="form-resetPass" method="POST" action="{{ route('password.email') }}">   
                         @if (session('status'))
                             <div class="alert alert-success" role="alert">
                                 {{ session('status') }}
@@ -25,41 +16,36 @@
                         @endif
                         @csrf
 
-                        <div class="row mb-3 justify-content-center">
-                            <!-- Contenedor flex para centrar los elementos horizontalmente -->
-                            <div class="col-md-10"> <!-- Columna con ancho específico -->
-                                <label style="margin-block-start: 20px" for="email"
-                                    class="form-label">{{ __('Correo electrónico') }}</label>
+                            <div class="form__section-resetPass"> 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3 justify-content-center">
-                            <!-- Contenedor flex para centrar los elementos horizontalmente -->
-                            <div class="col-md-10"> <!-- Columna con ancho específico -->
-                                <div class="txtEma">
-                                    <input style="" placeholder="Correo electrónico" id="email"
-                                        type="email"
-                                        class="form-control @error('email')  is-invalid @enderror form_inputResetPass"
+                                <label for="email"class="">{{ __('Correo electrónico') }}</label>
+                                
+                                <input placeholder="Correo electrónico" id="email" type="email"class="form-control @error('email')   is-invalid @enderror form_inputResetPass"
                                         name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-                                </div>
+                         
                             </div>
-                        </div>
-
-                        <div class="row justify-content-center">
-                            <!-- Contenedor flex para centrar los elementos horizontalmente -->
-                            <div class="col-md-10"> <!-- Columna con ancho específico -->
+                
+                           
+                            <div class="form__section-resetPass">
                                 <button type="submit" class="btnEmail">Enviar link de restablecimiento</button>
                             </div>
-                        </div>
+                    
                     </form>
+                    <div class="form__img-resetPass">
+                        <div class="img__container-resetPass">
+                            <div>
+                                <img src="../images/recursos/email_green.png" alt="">
+                            </div>
+                        </div>
+                        <div class="title__form-resetPass">{{ __('Restablecer contraseña') }}</div>
+                    </div>
                    
-                </div>
             </div>
+            
         </div>
     </div>
 @endsection
