00c03c9ba9e7e01d2c77ed777fe7961da6f021c5
JAAP2104 <almeidaperezjosephalexis@gmail.com>
2024-04-03 08:40:04 -0500

    emil.blade.php aplicacion  de <i class="fa-solid fa-user-shield fa-lg" style="color: #642c78; margin:5px"></i> en el boton

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -6,46 +6,51 @@
         style="background-image: url('{{ asset('images/fondoBrief4.jpg') }}'); background-position: center; min-height: 100vh; margin-block-start: -190px">
         <div class="line__email-pass">
             <div class="flex-direction">
-                
-                   
-                    <form class="form-resetPass" method="POST" action="{{ route('password.email') }}">   
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-                        @csrf
 
-                            <div class="form__section-resetPass"> 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                                <label for="email"class="">{{ __('Correo electrónico') }}</label>
-                                
-                                <input placeholder="Correo electrónico" id="email" type="email"class="form-control @error('email')   is-invalid @enderror form_inputResetPass"
-                                        name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-                         
-                            </div>
-                
-                           
-                            <div class="form__section-resetPass">
-                                <button type="submit" class="btnEmail">Enviar link de restablecimiento</button>
-                            </div>
-                    
-                    </form>
-                    <div class="form__img-resetPass">
-                        <div class="img__container-resetPass">
-                            <div>
-                                <img src="../images/recursos/email_green.png" alt="">
-                            </div>
+
+                <form class="form-resetPass" method="POST" action="{{ route('password.email') }}">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
                         </div>
-                        <div class="title__form-resetPass">{{ __('Restablecer contraseña') }}</div>
+                    @endif
+                    @csrf
+
+                    <div class="form__section-resetPass">
+                        @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                        <label for="email"class="label__resetPass">{{ __('Correo electrónico') }}</label>
+
+                        <input placeholder="Correo electrónico" id="email"
+                            type="email"class="form-control @error('email')   is-invalid @enderror form_inputResetPass"
+                            name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
                     </div>
-                   
+
+
+                    <div class="form__section-resetPass">
+                        <button type="submit" class="btnEmail">Enviar link de restablecimiento
+                            <i class="fa-solid fa-user-shield fa-lg" style="color: #642c78; margin:5px"></i>
+                        </button>
+                    </div>
+
+                </form>
+                <div class="form__img-resetPass">
+                    <div class="img__container-resetPass">
+                        <div>
+                            <img src="../images/recursos/email_green.png" alt="">
+                        </div>
+                    </div>
+                    <div class="title__form-resetPass">{{ __('Restablecer contraseña') }}
+                        
+                    </div>
+                </div>
+
             </div>
-            
+
         </div>
     </div>
 @endsection
