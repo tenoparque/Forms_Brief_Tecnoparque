edff2f21faa937e703a52c3d7a4bff81694f8edc
Darienpes29 <darienpesca29@gmail.com>
2024-04-08 16:42:10 -0500

    Se modificó la vista de 'Reportes y Estadísticas'

--- resources/views/reportes-estadisticas/reports.blade.php
+++ resources/views/reportes-estadisticas/reports.blade.php
@@ -27,8 +27,31 @@
             </div>
             <div class="">
                 <div class="row mb-3">
-                    <div class="col d-flex justify-content-between align-items-center">
-                        
+                    <div class="col-md-4">
+                        <select id="slcReport" title="Seleccionar Reporte" required
+                            style="width: 100%; border-radius: 50px; border-style: solid; border-width:4px; border-color: #ececec; background-color:  #ececec;height:45px;  margin-bottom: 10px; padding-left: 5px">
+                            <option value="" disabled selected>Seleccione el tipo de reporte a generar...</option>
+                            <option value="Solicitud">Solicitud</option>
+                            <option value="Nodo">Nodo</option>
+                            <option value="Historial de Modificaciones">Historial de Modificaciones</option>
+                            <option value="Diseñador">Diseñador</option>
+                        </select>
+                    </div>
+                    <div class="col-md-4">
+                        <select id="slcTipoDato" title="Seleccionar TipoDato" required
+                            style="width: 100%; border-radius: 50px; border-style: solid; border-width:4px; border-color: #ececec; background-color:  #ececec;height:45px;  margin-bottom: 10px; padding-left: 5px">
+                            <option value="" disabled selected>Seleccione el tipo de dato a filtrar...</option>
+                        </select>
+                    </div>
+                    <div class="col-md-3">
+                        <select title="Seleccionar DatoxTipo" required
+                            style="width: 100%; border-radius: 50px; border-style: solid; border-width:4px; border-color: #ececec; background-color:  #ececec;height:45px;  margin-bottom: 10px; padding-left: 5px">
+                            <option value="" disabled selected>Seleccione el dato x tipo de dato...</option>
+                        </select>
+                    </div>
+                    <div class="col-md-1">
+                        <a href="{{ route('solicitudes.pdf') }}" class="btnpdf" target="_blank"><i
+                                class="fa-solid fa-file-pdf fa-2xl" style="color: #642c78; margin-left: -60px;"></i></a>
                     </div>
                 </div>
             </div>
@@ -72,5 +95,33 @@
                 }
             });
         })
+
+        document.getElementById('slcReport').addEventListener('change', function() {
+            var slcReport = this.value;
+            var slcTipoDato = document.getElementById('slcTipoDato');
+
+            // Limpia las opciones existentes
+            slcTipoDato.innerHTML = "";
+
+            var opcionPorDefecto = document.createElement('option');
+            opcionPorDefecto.text = 'Seleccione el tipo de dato a filtrar...';
+            opcionPorDefecto.value = '';
+            opcionPorDefecto.disabled = true;
+            opcionPorDefecto.selected = true;
+            slcTipoDato.add(opcionPorDefecto);
+
+            if (slcReport == 'Solicitud') {
+                // Definimos las opciones que queremos agregar
+                var opciones = ['Tipo de Solicitud', 'Nodo', 'Evento', 'Estado'];
+
+                // Agrega las opciones al segundo select
+                for (var i = 0; i < opciones.length; i++) {
+                    var opcion = document.createElement('option');
+                    opcion.text = opciones[i];
+                    opcion.value = opciones[i];
+                    slcTipoDato.add(opcion);
+                }
+            }
+        });
     </script>
 @endsection
