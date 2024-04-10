edba2501e2f06628fee67257d7616b2e70a2f052
Darienpes29 <darienpesca29@gmail.com>
2024-04-10 15:05:17 -0500

    .

--- resources/views/user/miperfil.blade.php
+++ resources/views/user/miperfil.blade.php
@@ -7,94 +7,107 @@
 @section('content')
     <link rel="stylesheet" href="{{ asset('css/slayouts.css') }}">
 
-
-    <section class="container shadow p-4 my-5 bg-light rounded">
-        <div class="container">
+    <div class="row p-3">
+        <div class="col-md-6">
+            <div class="form-group">
+                <label style="font-size: 16px;  color:black">Nombres</label>
+                <input type="text" name="name" class="form-control" placeholder="Nombres" style="width: 100%; height:45px; border-radius: 50px; border-style: solid; border-color: #ececec; background-color:  #ececec ; margin-bottom: 10px; margin-top:8px">
+            </div>
+    
+            <div class="form-group">
+                <label style="font-size: 16px;  color:black">Correo</label>
+                <input type="text" name="email" class="form-control" placeholder="Correo" style="width: 100%; height:45px; border-radius: 50px; border-style: solid; border-color: #ececec; background-color:  #ececec; margin-bottom: 10px; margin-top:8px">
+            </div>
+        </div>
+        <div class="col-md-6">
+    
+            <div class="form-group">
+                <label style="font-size: 16px;  color:black">Apellidos</label>
+                <input type="text" name="apellidos" class="form-control" placeholder="Apellidos" style="width: 100%; height:45px; border-radius: 50px; border-style: solid; border-color: #ececec; background-color:  #ececec; margin-bottom: 10px; margin-top:8px">
+            </div>
+    
+            <div class="form-group">
+                <label style="font-size: 16px;  color:black">Celular</label>
+                <input type="text" name="celular" class="form-control" placeholder="Celular" style="width: 100%; height:45px; border-radius: 50px; border-style: solid; border-color: #ececec; background-color:  #ececec; margin-bottom: 10px; margin-top:8px">
+            </div>
+    
+        </div>
+        <div class="col-md-12">
             <div class="row">
-                <div class="col-sm-12">
-                    <div class="card-header">
-                        <div style="d-flex justify-content-between align-items-center">
-
-                            <div class="d-flex mt-3 mb-4">
-                                <div>
-                                    <h1 class="primeraPalabraFlex"
-                                        style="margin-right: 0; font-size: 180%; font-weight: 900; color: rgb(0, 49, 77)">
-                                        {{ __('USUARIOS REGISTRADOS') }}</h1>
+                <div class="col-md-4">
+    
+                    <label style="font-size: 16px;  color: ; font-weight: bold">Nodo</label>
+                    <div style="position: relative;">
+                        <select name="id_nodo" class="form-control" style="width: 100%; height:45px; border-radius: 50px; border-style: solid; border-color: #ececec; background-color:  #ececec; margin-bottom: 10px; margin-top:8px">
+                            <!-- Opciones del select -->
+                        </select>
+                        <div class="icono" onclick="toggleSelect()">
+                            <div class="circle-play">
+                                <div class="circle"></div>
+                                <div class="triangle"></div>
+                            </div>
+                        </div>
+                    </div>
+    
+                </div>
+    
+                <div class="col-md-4">
+                    <div class="form-group">
+                        <label style="font-size: 16px;  color:black">Estado</label>
+                        <div style="position: relative;">
+                            <select name="id_estado" class="form-control" style="width: 100%; height:45px; border-radius: 50px; border-style: solid; border-color: #ececec; background-color:  #ececec; margin-bottom: 10px; margin-top:8px">
+                                <!-- Opciones del select -->
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="form-group">
+                        <label style="font-size: 16px;  color:black" for="role">Roles</label>
+                        <div style="position: relative; width: 100%;">
+                            <select
+                                style="width: 100%; height:45px; border-radius: 50px; border-color: #ececec; background-color:  #ececec; margin-bottom: 10px; margin-top:8px; padding-right: 30px; -webkit-appearance: none; -moz-appearance: none; appearance: none;"
+                                name="role" id="role" class="form-control">
+                                <!-- Opciones del select -->
+                            </select>
+                            <div class="icono" onclick="toggleSelect()">
+                                <div class="circle-play">
+                                    <div class="circle"></div>
+                                    <div class="triangle"></div>
                                 </div>
                             </div>
                         </div>
-                    </div>                   
-                    <div class="card-body">
-                        <div class="row mb-3">
-                            <div class="col d-flex justify-content-between align-items-center">
-                                <input class="form-control" id="search" placeholder="Ingrese el email del usuario..."
-                                    style="width: 70%; border-radius: 50px; border-style: solid; border-width:5px; border-color: #DEE2E6">
-                                <a href="{{ route('register') }}" class="btnCrear">{{ __('CREAR') }}
-                                    <i class="fa-solid fa-circle-play iconDCR"></i></a>
-                            </div>
-                        </div>
-                        <div class="table-responsive"
-                            style="background-color: #DEE2E6; border-radius: 18px; border-style: solid; border-width:2px; border-color: #DEE2E6">
-                            <table class="table table-bordered table-hover">
-                                <thead class="thead-dark">
-                                    <tr style="border-width: 2px">
-                                        <th>Nombre</th>
-                                        <th>Apellidos</th>
-                                        <th>Email</th>
-                                        <th>Celular</th>
-                                        <th>Nodo</th>
-                                        <th>Estado</th>
-                                        <th>Rol</th>
-                                        <th>Opciones</th>
-                                    </tr>
-                                </thead>
-                                <tbody class="alldata">
-                                    @foreach ($user as $user)
-                                        <tr>
-
-                                            <td data-titulo="Nombre">{{ $user->name }}</td>
-                                            <td data-titulo="Apellidos">{{ $user->apellidos }}</td>
-                                            <td data-titulo="Email">{{ $user->email }}</td>
-                                            <td data-titulo="Celular">{{ $user->celular }}</td>
-                                            <td data-titulo="Nodo">{{ $user->nodo->nombre }}</td>
-
-                                            <td data-titulo="Estado">{{ $user->estado->nombre }}</td>
-                                            <td data-titulo="Rol">
-                                                @foreach ($user->roles as $role)
-                                                    {{ $role->name }}
-                                                    @if (!$loop->last)
-                                                        <br>
-                                                    @endif
-                                                @endforeach
-                                            </td>
-
-
-                                            <td id="buttoncell">
-                                                <form action="{{ route('users.destroy', $user->id) }}" method="POST">
-                                                    <a class="btnDetalle" href="{{ route('users.show', $user->id) }}"> 
-                                                        <i class="fa-sharp fa-solid fa-eye fa-xs iconDCR"></i>
-                                                        {{ __('Detalle') }}</a>
-
-                                                    <a class="btnDetalle" href="{{ route('users.edit', $user->id) }}">
-                                                        <i class="fa-solid fa-pen-to-square fa-xs iconEdit"></i>
-                                                        {{ __('Editar') }}</a>
-                                                </form>
-                                            </td>
-                                        </tr>
-                                    @endforeach
-                                </tbody>
-                                <!-- Another tbody is created for the search records -->
-                                <tbody id="Content" class="dataSearched">
-
-                                </tbody>
-                            </table>
-                        </div>
                     </div>
-
                 </div>
+                
+                <div class="col-md-12"
+                    style="align-content: center; margin-block-end: 15px; margin-block-start: 5px">
+                    <input type="checkbox" id="chkPassw" style="cursor: pointer"> ¿Desea actualizar su contraseña actual?</input>
+                </div>
+                
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label style="font-size: 16px;  color:black">Contraseña</label>
+                        <input type="text" id="txtPassw" class="form-control" placeholder="Contraseña" disabled style="width: 100%; height:45px; border-radius: 50px; border-style: solid; border-color: #ececec; background-color:  #ececec; margin-bottom: 10px; margin-top:8px">
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label style="font-size: 16px;  color:black">Confirmar contraseña</label>
+                        <input type="text" id="txtCPassw" class="form-control" placeholder="Confirmar contraseña" disabled style="width: 100%; height:45px; border-radius: 50px; border-style: solid; border-color: #ececec; background-color:  #ececec; margin-bottom: 10px; margin-top:8px">
+                    </div>
+                </div>
+                
             </div>
         </div>
-    </section>
+    </div>    
+    
+    <div class="col-md-12" style="text-align: right;">
+        <button type="submit" class="btnGuardar">
+            {{ __('GUARDAR') }}
+            <i class="fa-solid fa-circle-plus fa-sm iconDCR"></i>
+        </button>
+    </div>
 
     <!-- CSS Style -->
 
@@ -123,4 +136,27 @@
             border-right-width: 0;
         }
     </style>
+
+<script>
+    document.getElementById('chkPassw').addEventListener('click', function(){
+        var txtPassw = document.getElementById('txtPassw');
+        if(this.checked){
+            txtPassw.disabled = false;
+        } else {
+            txtPassw.disabled = true;
+            txtPassw.value = '';
+        }
+    });
+
+    document.getElementById('chkPassw').addEventListener('click', function(){
+        var txtCPassw = document.getElementById('txtCPassw');
+        if(this.checked){
+            txtCPassw.disabled = false;
+        } else {
+            txtCPassw.disabled = true;
+            txtCPassw.value = '';
+        }
+    });
+</script>
+
 @endsection
