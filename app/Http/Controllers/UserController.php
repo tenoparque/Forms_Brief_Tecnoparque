3dc694dbcfa9fadda957b9b28c6b6c3da3f7e418
Brandon <brandon.buendia@jala.university>
2024-04-15 10:35:51 -0500

    ya se actualiza la informacion completa

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -172,7 +172,8 @@ class UserController extends Controller
     public function update(Request $request)
     {
         // Validar los datos del formulario si es necesario
-    
+    // Recuerda encriptar la contraseña
+        // dd($request->input('txtPassw'));
         // Obtener el usuario actualmente autenticado
         $user = auth()->user();
     
@@ -183,9 +184,13 @@ class UserController extends Controller
         $user->celular = $request->input('celular');
         
         // Si se ha marcado la casilla para actualizar la contraseña
-        if ($request->has('chkPassw')) {
-            $user->password = bcrypt($request->input('txtPassw')); // Recuerda encriptar la contraseña
+        if ($request->filled('txtPassw')) {
+            // Encriptar la nueva contraseña y actualizarla
+            $user->password = Hash::make($request->input('txtPassw'));
         }
+    
+        // Aquí usamos dd() para verificar los datos antes de guardarlos en la base de datos
+        // dd($user);
     
         // Guardar los cambios en la base de datos
         $user->save();
@@ -194,6 +199,7 @@ class UserController extends Controller
         return redirect()->route('home.index')
             ->with('success', 'Usuario Actualizado Exitosamente');
     }
+    
 
     /**
      * @param int $id
