a4009b6afbec9769b246010032884e9a751ba339
Darienpes29 <darienpesca29@gmail.com>
2024-04-15 15:58:01 -0500

    Se modificó la vista del Detalle de la Ciudad

--- resources/views/ciudade/show.blade.php
+++ resources/views/ciudade/show.blade.php
@@ -5,14 +5,14 @@
 @endsection
 
 @section('content')
-
+<link rel="stylesheet" href="{{ asset('css/layout.css') }}">
     <section class="container shadow p-4 my-5 bg-light rounded">
         <div class="container">
             <div class="col-sm-12">
                 <div class="">
                     <div class="card-header">
                         <div class="float-left">
-                            <div class="d-flex mt-3 mb-4">
+                            <div class="d-flex mt-3 mb-2">
                                 <div>
                                     <h1 class="primeraPalabraFlex" style="font-size: 200%">{{ __('DETALLE DE') }}</h1>
                                 </div>
@@ -24,7 +24,8 @@
                     </div>
 
                     <div class="card-body">
-
+                        <div class="row mb-3">
+                        </div>
                         <div class="table-responsive"
                             style="background-color: #DEE2E6; border-radius: 18px; border-style: solid; border-width:2px; border-color: #DEE2E6">
                             <table class="table table-bordered table-hover">
@@ -51,12 +52,10 @@
 
                 </div>
             </div>
-            <div>
-                <a href="{{ route('ciudades.index') }}" class="btn btn-outline"
-                    style="color:#00324D; border:2px solid #82DEF0; height: 40px; width:130px; cursor: pointer; border-radius: 35px; margin-top:18px; justify-content: center; justify-items: center; margin-left: 90%;"
-                    onmouseover="this.style.backgroundColor='#b2ebf2';" onmouseout="this.style.backgroundColor='#FFFF';">
+            <div class="mt-3 d-flex justify-content-end">
+                <a href="{{ route('ciudades.index') }}" class="btnRegresar">
                     {{ __('REGRESAR') }}
-                    <i class="fa-solid fa-circle-play fa-flip-both" style="color: #642c78;"></i>
+                    <i class="fa-solid fa-circle-play fa-flip-both iconDCR"></i>
                 </a>
             </div>
         </div>
