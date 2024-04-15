1dd0ce808518691f3a9bbf1c112c9a7778927456
Darienpes29 <darienpesca29@gmail.com>
2024-04-15 15:53:27 -0500

    Se moidifcó la vista de Crear Departamento

--- resources/views/departamento/form.blade.php
+++ resources/views/departamento/form.blade.php
@@ -1,14 +1,10 @@
 <div class="box box-info padding-1">
-    .alert.alert-danger {
-        color: #642c78;
-    }
-    .invalid-feedback{
-        color:  #642c78;
-    }
+    <link rel="stylesheet" href="{{ asset('css/layout.css') }}">
     <div class="box-body">
+
         <div class="form-group">
             {{ Form::label('nombre', null, ['style' => 'font-size: 18px; font-weight: bold ; margin-left: 35px;']) }}
-            {{ Form::text('nombre', $departamento->nombre, ['class' => 'form-control' . ($errors->has('nombre') ? ' is-invalid' : ''), 'placeholder' => 'Nombre','style' => 'width: 95%; border-radius: 50px; border-style: solid; border-width:4px; border-color: #ececec; background-color:  #ececec;height:45px; margin-bottom: 10px; margin-left: 25px;']) }}
+            {{ Form::text('nombre', $departamento->nombre, ['class' => 'form-control' . ($errors->has('nombre') ? ' is-invalid' : ''), 'placeholder' => 'Nombre','style' => 'width: 95%; border-radius: 50px; border-style: solid; border-width:4px; border-color: #ececec; background-color:  #ececec;margin-bottom: 10px; margin-left: 25px;']) }}
             {!! $errors->first('nombre', '<div class="invalid-feedback">:message</div>') !!}
         </div>
     </div>
