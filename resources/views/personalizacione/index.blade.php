5231358d0ad0cee4cb91d47b9fc724e2f86ee7c7
JAAP2104 <almeidaperezjosephalexis@gmail.com>
2024-05-10 10:42:41 -0500

    se hizo cambios ene l css del tamaño de la imagen en la tabla personalizacion

--- resources/views/personalizacione/index.blade.php
+++ resources/views/personalizacione/index.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('content')
-<link rel="stylesheet" href="{{ asset('css/layout.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/layout.css') }}">
     <section class="container shadow p-4 my-5 bg-light rounded">
         <div class="container">
             <div class="row">
@@ -22,11 +22,11 @@
                                 </div>
                             </div>
                         </div>
-                    </div>               
+                    </div>
                     <div class="card-body">
                         <div class="table-responsive"
                             style="background-color: #DEE2E6; border-radius: 18px; border-style: solid; border-width:2px; border-color: #DEE2E6">
-                            <table class="table table-bordered table-hover" >
+                            <table class="table table-bordered table-hover">
                                 <thead class="thead-dark">
                                     <tr style="text-align: center">
                                         {{-- <th>No</th> --}}
@@ -42,83 +42,76 @@
                                         <th>Opciones</th>
                                     </tr>
                                 </thead>
-                                <tbody class="alldata" >
+                                <tbody class="alldata">
                                     @foreach ($personalizaciones as $personalizacione)
-                                        <tr >
+                                        <tr>
                                             {{-- <td>{{ ++$i }}</td> --}}
-                                            <td data-titulo="Logo">
-                                               <div class="logoPersonalizacion" style="">
-                                                <img src="data:image/png;base64,{{ base64_encode($personalizacione->logo) }}"
-                                                alt="LOGO" class="ImgCeldaPesonalizacion" >
+                                            <td data-titulo="Logo" class="tdlogoImgPerson">
+                                                <div class="logoPersonalizacion" style="">
+                                                    <img src="data:image/png;base64,{{ base64_encode($personalizacione->logo) }}"
+                                                        alt="LOGO" class="ImgCeldaPesonalizacion">
                                             </td>
-                                               </div>
-                                            <td data-titulo="Color Principal">
-                                                <div class="PersonalizacionColor" >
-                                                    <div class="ChildrenPersonalizacion"
-                                                        style="width: 20px; height: 20px; background-color: {{ $personalizacione->color_principal }}; ">
-                                                    </div>
-                                                    {{ $personalizacione->color_principal }}
-                                                </div>
-                                            </td>
-                                            <td data-titulo="Color Secundario" >
-                                                <div class="PersonalizacionColor">
-                                                    <div class="ChildrenPersonalizacion"
-                                                        style="width: 20px; height: 20px; background-color: {{ $personalizacione->color_secundario }}; ">
-                                                    </div>
-                                                    {{ $personalizacione->color_secundario }}
-                                                </div>
-                                            </td>
-                                            <td data-titulo="Color Terciario">
-                                                <div class="PersonalizacionColor" >
-                                                    <div class="ChildrenPersonalizacion"
-
-                                                        style="width: 20px; height: 20px; background-color: {{ $personalizacione->color_terciario }}; ">
-                                                    </div>
-                                                    {{ $personalizacione->color_terciario }}
-                                                </div>
-                                            </td>
-                                            <td data-titulo="Color Cuarto">
-                                                <div class="PersonalizacionColor" >
-                                                    <div class="ChildrenPersonalizacion"
-
-                                                        style="width: 20px; height: 20px; background-color: {{ $personalizacione->color_cuarto }}; ">
-                                                    </div>
-                                                    {{ $personalizacione->color_cuarto }}
-                                                </div>
-                                            </td>
-                                            <td data-titulo="Email de Usuario">{{ $personalizacione->user->email }}</td>
-                                            <td data-titulo="Estado">{{ $personalizacione->estado->nombre }}</td>
-                                            <td id="buttoncell" >
-
-                                                <a href="{{ route('personalizaciones.show', $personalizacione->id) }}"
-                                                    class="btnDetalle"
-                                                    >
-                                                    <i class="fa-sharp fa-solid fa-eye fa-xs iconDCR"
-                                                        ></i>
-                                                    {{ __('Detalle') }}
-                                                    
-                                                </a>
-
-                                                <a href="{{ route('personalizaciones.edit', $personalizacione->id) }}"
-                                                    class="btnEdit"
-                                                    >
-                                                    <i class="fa-solid fa-pen-to-square fa-xs iconEdit" ></i>
-                                                    {{ __('Editar') }}
-                                                    
-                                                </a>
-
-
-                                            </td>
-                                        </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
                         </div>
-                    </div>
+                        <td data-titulo="Color Principal">
+                            <div class="PersonalizacionColor">
+                                <div class="ChildrenPersonalizacion"
+                                    style="width: 20px; height: 20px; background-color: {{ $personalizacione->color_principal }}; ">
+                                </div>
+                                {{ $personalizacione->color_principal }}
+                            </div>
+                        </td>
+                        <td data-titulo="Color Secundario">
+                            <div class="PersonalizacionColor">
+                                <div class="ChildrenPersonalizacion"
+                                    style="width: 20px; height: 20px; background-color: {{ $personalizacione->color_secundario }}; ">
+                                </div>
+                                {{ $personalizacione->color_secundario }}
+                            </div>
+                        </td>
+                        <td data-titulo="Color Terciario">
+                            <div class="PersonalizacionColor">
+                                <div class="ChildrenPersonalizacion"
+                                    style="width: 20px; height: 20px; background-color: {{ $personalizacione->color_terciario }}; ">
+                                </div>
+                                {{ $personalizacione->color_terciario }}
+                            </div>
+                        </td>
+                        <td data-titulo="Color Cuarto">
+                            <div class="PersonalizacionColor">
+                                <div class="ChildrenPersonalizacion"
+                                    style="width: 20px; height: 20px; background-color: {{ $personalizacione->color_cuarto }}; ">
+                                </div>
+                                {{ $personalizacione->color_cuarto }}
+                            </div>
+                        </td>
+                        <td data-titulo="Email de Usuario">{{ $personalizacione->user->email }}</td>
+                        <td data-titulo="Estado">{{ $personalizacione->estado->nombre }}</td>
+                        <td id="buttoncell">
 
+                            <a href="{{ route('personalizaciones.show', $personalizacione->id) }}" class="btnDetalle">
+                                <i class="fa-sharp fa-solid fa-eye fa-xs iconDCR"></i>
+                                {{ __('Detalle') }}
+
+                            </a>
+
+                            <a href="{{ route('personalizaciones.edit', $personalizacione->id) }}" class="btnEdit">
+                                <i class="fa-solid fa-pen-to-square fa-xs iconEdit"></i>
+                                {{ __('Editar') }}
+
+                            </a>
+
+
+                        </td>
+                        </tr>
+                        @endforeach
+                        </tbody>
+                        </table>
+                    </div>
                 </div>
-                {!! $personalizaciones->links() !!}
+
             </div>
+            {!! $personalizaciones->links() !!}
+        </div>
         </div>
     </section>
 @endsection
