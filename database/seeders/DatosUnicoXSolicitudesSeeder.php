966b4f94d8e617b45ec7a2a7660ce9886fac452f
Marly <rangelmarly@gmail.com>
2024-11-06 14:50:47 -0500

    Seeders de tipo de solicitudes y datos unicos

--- database/seeders/DatosUnicoXSolicitudesSeeder.php
+++ database/seeders/DatosUnicoXSolicitudesSeeder.php
@@ -23,9 +23,63 @@ class DatosUnicoXSolicitudesSeeder extends Seeder
             [
                 'nombre'=>'Titulo de pieza gráfica o evento',
                 'id_tipos_de_datos'=>1,
+                'id_tipos_de_solicitudes'=>1,
+                'id_estados'=>1
+            ],
+            [
+                'nombre'=>'Fecha del evento',
+                'id_tipos_de_datos'=>1,
+                'id_tipos_de_solicitudes'=>1,
+                'id_estados'=>1
+            ],
+            [
+                'nombre'=>'Hora del evento',
+                'id_tipos_de_datos'=>1,
+                'id_tipos_de_solicitudes'=>1,
+                'id_estados'=>1
+            ],
+            [
+                'nombre'=>'Texto que desea que tenga la pieza',
+                'id_tipos_de_datos'=>1,
+                'id_tipos_de_solicitudes'=>1,
+                'id_estados'=>1
+            ],
+            [
+                'nombre'=>'Observaciones generales',
+                'id_tipos_de_datos'=>1,
                 'id_tipos_de_solicitudes'=>2,
                 'id_estados'=>1
-            ]
+            ],
+            [
+                'nombre'=>'Nombre Del Producto',
+                'id_tipos_de_datos'=>1,
+                'id_tipos_de_solicitudes'=>3,
+                'id_estados'=>1
+            ],
+            [
+                'nombre'=>'Descripción del proyecto ',
+                'id_tipos_de_datos'=>1,
+                'id_tipos_de_solicitudes'=>3,
+                'id_estados'=>1
+            ],
+            [
+                'nombre'=>'Grupo Objetivo',
+                'id_tipos_de_datos'=>1,
+                'id_tipos_de_solicitudes'=>3,
+                'id_estados'=>1
+            ],
+            [
+                'nombre'=>'Cubrimiento y alcance que tiene el proyecto(Local, Regional, Nacional, Internacional)',
+                'id_tipos_de_datos'=>1,
+                'id_tipos_de_solicitudes'=>3,
+                'id_estados'=>1
+            ],
+            [
+                'nombre'=>'Tamaño de la empresa(Grande, Mediana, Micro-empresa)',
+                'id_tipos_de_datos'=>1,
+                'id_tipos_de_solicitudes'=>3,
+                'id_estados'=>1
+            ],
         ]);
     }
 }
--- database/seeders/ServiciosXTiposSolicitudesSeeder.php
+++ database/seeders/ServiciosXTiposSolicitudesSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -16,9 +15,124 @@ class ServiciosXTiposSolicitudesSeeder extends Seeder
         DB::table('servicios_por_tipos_de_solicitudes')->insert([
             [
                 'nombre'=>'Pieza para Instagram y Facebook (1080 x 1080) px',
+                'id_tipo_de_solicitud'=>1,
+                'id_estado'=>1
+            ],
+            [
+                'nombre'=>'Pieza para estados Whatsapp (1080 x 1920) px',
+                'id_tipo_de_solicitud'=>1,
+                'id_estado'=>1
+            ],
+            [
+                'nombre'=>'Pieza para historias en redes sociales (750 x 1334) px',
+                'id_tipo_de_solicitud'=>1,
+                'id_estado'=>1
+            ],
+            [
+                'nombre'=>'Banner para web (1024x769) px',
+                'id_tipo_de_solicitud'=>1,
+                'id_estado'=>1
+            ],
+            [
+                'nombre'=>'Piezas para correo (320 x 550) px',
+                'id_tipo_de_solicitud'=>1,
+                'id_estado'=>1
+            ],
+            [
+                'nombre'=>'Pendón (determinar tamaño)',
+                'id_tipo_de_solicitud'=>1,
+                'id_estado'=>1
+            ],
+            [
+                'nombre'=>'Afiche (determinar tamaño)',
+                'id_tipo_de_solicitud'=>1,
+                'id_estado'=>1
+            ],
+            [
+                'nombre'=>'Infografía (determinar tamaño)',
+                'id_tipo_de_solicitud'=>1,
+                'id_estado'=>1
+            ],
+            [
+                'nombre'=>'Agregar noticia al micrositio del nodo',
+                'id_tipo_de_solicitud'=>1,
+                'id_estado'=>1
+            ],
+            [
+                'nombre'=>'Agregar evento al micrositio del nodo',
                 'id_tipo_de_solicitud'=>2,
                 'id_estado'=>1
-            ]
+            ],
+            [
+                'nombre'=>'Cambiar imagen o video Banner Principal nodo',
+                'id_tipo_de_solicitud'=>2,
+                'id_estado'=>1
+            ],
+            [
+                'nombre'=>'Agregar aliado estrategico del nodo',
+                'id_tipo_de_solicitud'=>2,
+                'id_estado'=>1
+            ],
+            [
+                'nombre'=>'Cambiar informacion canales de información',
+                'id_tipo_de_solicitud'=>2,
+                'id_estado'=>1
+            ],
+            [
+                'nombre'=>'Cambiar información lineas tecnologicas',
+                'id_tipo_de_solicitud'=>2,
+                'id_estado'=>1
+            ],
+            [
+                'nombre'=>'Cambiar informacion de sub-lineas tecnologicas',
+                'id_tipo_de_solicitud'=>2,
+                'id_estado'=>1
+            ],
+            [
+                'nombre'=>'Crear modulo especial para evento especializado del nodo.',
+                'id_tipo_de_solicitud'=>2,
+                'id_estado'=>1
+            ],
+            [
+                'nombre'=>'Otra',
+                'id_tipo_de_solicitud'=>2,
+                'id_estado'=>1
+            ],
+            [
+                'nombre'=>'Diseño de marca',
+                'id_tipo_de_solicitud'=>3,
+                'id_estado'=>1
+            ],
+            [
+                'nombre'=>'Diseño de Identidad Corporativa',
+                'id_tipo_de_solicitud'=>3,
+                'id_estado'=>1
+            ],
+            [
+                'nombre'=>'Etiqueta',
+                'id_tipo_de_solicitud'=>3,
+                'id_estado'=>1
+            ],
+            [
+                'nombre'=>'Diseño de empaque',
+                'id_tipo_de_solicitud'=>3,
+                'id_estado'=>1
+            ],
+            [
+                'nombre'=>'Diseño de la ficha Técnica de producto',
+                'id_tipo_de_solicitud'=>3,
+                'id_estado'=>1
+            ],
+            [
+                'nombre'=>'Edición fotográfica',
+                'id_tipo_de_solicitud'=>3,
+                'id_estado'=>1
+            ],
+            [
+                'nombre'=>'Otra',
+                'id_tipo_de_solicitud'=>3,
+                'id_estado'=>1
+            ],
         ]);
     }
 }
