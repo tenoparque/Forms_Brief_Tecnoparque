fa1e5deeb2cf2ed6771ebcfdb6afb41a3c4f6e92
Marly <rangelmarly@gmail.com>
2024-11-10 18:51:38 -0500

    Se cambio el diseño de las cards del conteo de solicitudes y se subieron las graficas de dona de primeras

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -44,66 +44,88 @@
                         <div class="container-fluid py-5">
                             <div class="row">
                                 <div class="col-xl-4 col-sm-6 mb-xl-12 mb-4">
-                                    <div class="card  border-light shadow">
-                                        <div class="card-header p-3 pt-2"style="border:transparent;">
-                                            <div
-                                                class="icon icon-lg icon-shape bg-gradient-success shadow-primary text-center border-radius-xl mt-n4 position-absolute">
-                                                <i class="fa-solid fa-envelope-circle-check"></i>
-                                            </div>
-                                            <div class="text-end pt-1">
-                                                <p class="titulosoli">Solicitudes</p>
+                                    <div class="card border-light shadow">
+                                        <div class="card-header p-3 pt-2" style="border:transparent;">
+                                            <div class="d-flex align-items-center">
+                                                <!-- Ícono con el número al lado -->
+                                                <div class="icon icon-lg icon-shape bg-gradient-success shadow-primary text-center border-radius-xl me-3">
+                                                    <i class="fa-solid fa-envelope-circle-check"></i>
+                                                </div>
+                                                <!-- Texto y número más grande -->
+                                                <div>
+                                                    <p class="titulosoli mb-0">Solicitudes</p>
+                                                    <span class="numero-color display-5" id="cardUno"></span> <!-- Número en tamaño grande -->
+                                                </div>
                                             </div>
                                         </div>
-
-                                        <div class="card-footer p-3" style="background-color:#ececee">
-                                            <span class="total-color">Total:</span>
-                                            <span class="numero-color" id="cardUno"></span>
-                                        </div>
-
                                     </div>
                                 </div>
+                                
                                 @if (!auth()->user()->hasRole('Designer|Experto Divulgación'))
-                                    <div class="col-xl-4 col-sm-6 mb-xl-12 mb-4">
-                                        <div class="card  border-light shadow">
-                                            <div class="card-header p-3 pt-2"style="border:transparent;">
-                                                <div
-                                                    class="icon icon-lg icon-shape bg-gradient-dark shadow-primary text-center border-radius-xl mt-n4 position-absolute">
+                                <div class="col-xl-4 col-sm-6 mb-xl-12 mb-4">
+                                    <div class="card border-light shadow">
+                                        <div class="card-header p-3 pt-2" style="border:transparent;">
+                                            <div class="d-flex align-items-center">
+                                                <!-- Ícono con el número al lado -->
+                                                <div class="icon icon-lg icon-shape bg-gradient-dark shadow-primary text-center border-radius-xl me-3">
                                                     <i class="fa-solid fa-envelope-open-text"></i>
-
                                                 </div>
-                                                <div class="text-end pt-1">
-                                                    <p class="titulosoli">Solicitudes en espera</p>
+                                                <!-- Texto y número más grande -->
+                                                <div>
+                                                    <p class="titulosoli mb-0">Solicitudes en espera</p>
+                                                    <span class="numero-color display-5" id="cardDos"></span> <!-- Número en tamaño grande -->
                                                 </div>
                                             </div>
-                                            <div class="card-footer p-3" style="background-color:#ececee">
-                                                <span class="total-color">Total:</span>
-                                                <span class="numero-color" id="cardDos"></span>
-                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                                @endif
 
+                                <div class="col-xl-4 col-sm-6 mb-xl-12 mb-4">
+                                    <div class="card border-light shadow">
+                                        <div class="card-header p-3 pt-2" style="border:transparent;">
+                                            <div class="d-flex align-items-center">
+                                                <!-- Ícono con el número al lado -->
+                                                <div class="icon icon-lg icon-shape bg-gradient-info shadow-primary text-center border-radius-xl me-3">
+                                                    <i class="fa-solid fa-envelope-circle-check"></i>
+                                                </div>
+                                                <!-- Texto y número más grande -->
+                                                <div>
+                                                    <p class="titulosoli mb-0">Solicitudes Finalizadas</p>
+                                                    <span class="numero-color display-5" id="cardTres"></span> <!-- Número en tamaño grande -->
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                            @endcan
+                            <!-- Fin de las tarjetas de estadísticas -->
+                            <!-- Tarjetas con graficos Donas -->
+                            <div class="row mt-4">
+                                <div class="col-md-6">
+                                    <div class="card bg-crema  border-light"
+                                        style="display: flex;justify-content: center;align-items: center;flex-direction: column;text-align: center;">
+                                        <div class="card-body">
+                                            <h5 style="color:#00324d;" class="card-title">Proporción Solicitudes </h5>
+                                            <canvas id="grafico_solicitudes_modificaciones" style="width: 100%;"></canvas>
+                                        </div>
+                                    </div>
+                                </div>
+                                @if (Auth::user()->hasAnyRole(['Super Admin', 'Activador Nacional', 'Designer', 'Experto Divulgación']))
+                                    <div class="col-md-6">
+                                        <div class="card bg-crema border-light"
+                                            style="display: flex; justify-content: center; align-items: center; flex-direction: column; text-align: center;">
+                                            <div class="card-body">
+                                                <h5 style="color:#00324d" class="card-title">Tipos de solicitudes</h5>
+                                                <canvas id="grafico_tipos_solicitudes" style="width: 100%;"></canvas>
+                                            </div>
                                         </div>
                                     </div>
                                 @endif
 
-                                <div class="col-xl-4 col-sm-6 mb-xl-12 mb-4">
-                                    <div class="card  border-light shadow">
-                                        <div class="card-header p-3 pt-2"style="border:transparent;">
-                                            <div
-                                                class="icon icon-lg icon-shape bg-gradient-info shadow-primary text-center border-radius-xl mt-n4 position-absolute">
-                                                <i class="fa-solid fa-envelope-circle-check"></i>
-                                            </div>
-                                            <div class="text-end pt-1">
-                                                <p class="titulosoli">Solicitudes Finalizadas</p>
-                                            </div>
-                                        </div>
-                                        <div class="card-footer p-3" style="background-color:#ececee">
-                                            <span class="total-color">Total:</span>
-                                            <span class="numero-color" id="cardTres"></span>
-                                        </div>
-                                    </div>
-                                </div>
-                            @endcan
-                            <!-- Fin de las tarjetas de estadísticas -->
-
+                            </div>
                             <!-- Gráfico con Chart.js -->
                             @can('dashboard.charts')
                                 <div class="row mt-4" style="margin-top: 50px;margin-bottom: 30px">
@@ -117,54 +139,30 @@
                                 </div>
 
                                 @if (Auth::user()->hasAnyRole(['Super Admin', 'Activador Nacional']))
-                                <div class="row mt-4">
-                                    <div class="col-md-12">
-                                        <div class="card border-0">
-                                            <div class="card-body">
-                                                <canvas id="garfica_nodos_solicitudes" width="400" height="100"></canvas>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                
-
-                                <div class="row mt-4">
-                                    <div class="col-md-12">
-                                        <div class="card border-0">
-                                            <div class="card-body">
-                                                <canvas id="grafica_cantidades_asignadas" width="400"
-                                                    height="100"></canvas>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                @endif
-                                <!-- Fin del Gráfico con Chart.js -->
-
-                                <!-- Tarjetas con graficos -->
-                                <div class="row mt-4">
-                                    <div class="col-md-6">
-                                        <div class="card bg-crema  border-light"
-                                            style="display: flex;justify-content: center;align-items: center;flex-direction: column;text-align: center;">
-                                            <div class="card-body">
-                                                <h5 style="color:#00324d;" class="card-title">Proporción Solicitudes </h5>
-                                                <canvas id="grafico_solicitudes_modificaciones" style="width: 100%;"></canvas>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    @if (Auth::user()->hasAnyRole(['Super Admin', 'Activador Nacional', 'Designer', 'Experto Divulgación']))
-                                        <div class="col-md-6">
-                                            <div class="card bg-crema border-light"
-                                                style="display: flex; justify-content: center; align-items: center; flex-direction: column; text-align: center;">
+                                    <div class="row mt-4">
+                                        <div class="col-md-12">
+                                            <div class="card border-0">
                                                 <div class="card-body">
-                                                    <h5 style="color:#00324d" class="card-title">Tipos de solicitudes</h5>
-                                                    <canvas id="grafico_tipos_solicitudes" style="width: 100%;"></canvas>
+                                                    <canvas id="garfica_nodos_solicitudes" width="400"
+                                                        height="100"></canvas>
                                                 </div>
                                             </div>
                                         </div>
-                                    @endif
+                                    </div>
 
-                                </div>
+
+                                    <div class="row mt-4">
+                                        <div class="col-md-12">
+                                            <div class="card border-0">
+                                                <div class="card-body">
+                                                    <canvas id="grafica_cantidades_asignadas" width="400"
+                                                        height="100"></canvas>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                @endif
+                                <!-- Fin del Gráfico con Chart.js -->
                             @endcan
                             <!-- Fin de las tarjetas de estadísticas -->
 
@@ -359,7 +357,7 @@
                             grafico_tipos_solicitudes.update();
                         }
                     }
-                    
+
                     // Función para formatear la fecha en formato "mes año"
                     function formatearFecha(anio, mes) {
                         const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre',
